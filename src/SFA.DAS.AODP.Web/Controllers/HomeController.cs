637c14f52d42ceb3992a1d3834acc78080ec319b
Sean Brogan <sean.brogan@fujitsu.com>
2025-01-07 16:47:50 +0000

    Added form builder component

--- src/SFA.DAS.AODP.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.AODP.Web/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace SFA.DAS.AODP.Web.Controllers
             return View();
         }
 
+        public IActionResult FormBuilder()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
