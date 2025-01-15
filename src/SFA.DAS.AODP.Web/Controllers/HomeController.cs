020fbec04ac4f05d7a84e3dcffd2ffc2fe637b94
Sean Brogan <sean.brogan@fujitsu.com>
2025-01-15 12:40:06 +0000

    Removed redundant form builder endpoint from home controller and removed view

--- src/SFA.DAS.AODP.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.AODP.Web/Controllers/HomeController.cs
@@ -19,64 +19,10 @@ namespace SFA.DAS.AODP.Web.Controllers
             return View();
         }
 
-        public IActionResult FormBuilder()
-        {
-            return View();
-        }
-
-        //[HttpPost("/home/update")]
-        //public IActionResult Update([FromBody] string value)
-        //{
-        //    try
-        //    {
-        //        Console.WriteLine(value);
-        //        var x = 0;
-        //        return Ok();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Update()
-        {
-            var rawRequestBody = await GetRawBodyAsync(Request);
-
-            // Other code herereturn Ok();
-
-            return Ok();
-        }
-
-
-        public async Task<string> GetRawBodyAsync(
-            HttpRequest request,
-            Encoding encoding = null)
-        {
-            if (!request.Body.CanSeek)
-            {
-                // We only do this if the stream isn't *already* seekable,
-                // as EnableBuffering will create a new stream instance
-                // each time it's called
-                request.EnableBuffering();
-            }
-
-            request.Body.Position = 0;
-
-            var reader = new StreamReader(request.Body, encoding ?? Encoding.UTF8);
-
-            var body = await reader.ReadToEndAsync().ConfigureAwait(false);
-
-            request.Body.Position = 0;
-
-            return body;
         }
     }
 }
