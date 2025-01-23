0e5e1eac054002d0df4cb45ddc72cc10ee888722
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-01-23 15:41:31 +0000

    wip

--- src/SFA.DAS.AODP.Web/Models/Routing/CreateRouteViewModel.cs
+++ src/SFA.DAS.AODP.Web/Models/Routing/CreateRouteViewModel.cs
@@ -1,32 +1,50 @@
 ﻿namespace SFA.DAS.AODP.Web.Models.Routing
 {
+    public class SectionInformation
+    {
+        public Guid Key { get; set; }
+        public string Title { get; set; }
+        public int Order { get; set; }
+    }
+
+    public class PageInformation
+    {
+        public Guid Key { get; set; }
+        public string Title { get; set; }
+        public int Order { get; set; }
+    }
+
+    public class QuestionInformation
+    {
+        public Guid Key { get; set; }
+        public string Title { get; set; }
+        public int Order { get; set; }
+    }
+
+
     public class CreateRouteChooseSectionViewModel
     {
         public Guid FormVersionId { get; set; }
-        public Guid ChosenSectionId { get; set; }
+        public Guid ChosenSectionKey { get; set; }
         public List<SectionInformation> Sections { get; set; }
 
-        public class SectionInformation
-        {
-            public Guid Key { get; set; }
-            public string Title { get; set; }
-            public int Order { get; set; }
-        }
+    }
+
+    public class RouteInformation
+    {
+        public Guid OptionKey { get; set; }
+        public string NextPageKey { get; set; }
+        public string NextSectionKey { get; set; }
     }
 
     public class CreateRouteChoosePageViewModel
     {
         public Guid FormVersionId { get; set; }
         public Guid SectionKey { get; set; }
-        public Guid ChosenPageId { get; set; }
+        public Guid ChosenPageKey { get; set; }
         public List<PageInformation> Pages { get; set; }
 
-        public class PageInformation
-        {
-            public Guid Key { get; set; }
-            public string Title { get; set; }
-            public int Order { get; set; }
-        }
+
     }
 
     public class CreateRouteChooseQuestionViewModel
@@ -34,51 +52,38 @@
         public Guid FormVersionId { get; set; }
         public Guid SectionKey { get; set; }
         public Guid PageKey { get; set; }
-        public Guid ChosenQuestionId { get; set; }
-        public List<QuestionInformation> Questions { get; set; }
+        public Guid ChosenQuestionKey { get; set; }
 
-        public class QuestionInformation
-        {
-            public Guid Key { get; set; }
-            public string Title { get; set; }
-            public int Order { get; set; }
-        }
+        public List<QuestionInformation> Questions { get; set; }
+        public List<SectionInformation> Sections { get; set; }
+
     }
 
     public class CreateRouteViewModel
     {
         public Guid FormVersionId { get; set; }
-        public Guid ChosenQuestionId { get; set; }
+        public Guid SectionKey { get; set; }
+        public Guid PageKey { get; set; }
+        public Guid QuestionKey { get; set; }
+        public List<PageInformation> Pages { get; set; }
 
-        public List<SectionInformation> Sections { get; set; }
+        public List<RouteInformation> Routes { get; set; }
 
-        public class SectionInformation
+        public List<OptionInformation> Options { get; set; }
+
+        public class OptionInformation
         {
-            public Guid SectionId { get; set; }
+            public Guid OptionKey { get; set; }
             public string Title { get; set; }
             public int Order { get; set; }
 
-            public List<PageInformation> Pages { get; set; }
         }
-        public class PageInformation
-        {
-            public Guid PageId { get; set; }
-            public string Title { get; set; }
-            public int Order { get; set; }
+    }
 
-            public List<QuestionInformation> Questions { get; set; }
-
-
-
-        }
-
-        public class QuestionInformation
-        {
-            public Guid QuestionId { get; set; }
-            public string Title { get; set; }
-        }
-
-
+    public class ListRoutesViewModel
+    {
+        public Guid FormVersionId { get; set; }
+        public Dictionary<QuestionInformation, List<RouteInformation>> QuestionRoutes { get; set; } = new();
 
     }
 }
