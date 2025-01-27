0b4fc9c9543d0283c1f70172991c22bbf122d9d0
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-01-27 10:49:39 +0000

    Reapply "bug fixes"
    This reverts commit 0a93e5738863f28c1dad4d510d5b4c2acb7d4db9.

--- src/SFA.DAS.AODP.Web/Models/Section/EditSectionViewModel.cs
+++ src/SFA.DAS.AODP.Web/Models/Section/EditSectionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using SFA.DAS.AODP.Application.Queries.FormBuilder.Sections;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SFA.DAS.AODP.Web.Models.Section
 {
@@ -9,6 +10,25 @@ namespace SFA.DAS.AODP.Web.Models.Section
         public string Description { get; set; }
         public Guid FormVersionId { get; set; }
         public Guid SectionId { get; set; }
+        public List<Page> Pages { get; set; }
+        public class Page
+        {
+            public Guid Id { get; set; }
+            public Guid Key { get; set; }
+            public int Order { get; set; }
+            public string Title { get; set; }
+
+            public static implicit operator Page(GetSectionByIdQueryResponse.Page entity)
+            {
+                return new()
+                {
+                    Id = entity.Id,
+                    Key = entity.Key,
+                    Order = entity.Order,
+                    Title = entity.Title
+                };
+            }
+        }
 
 
         public static EditSectionViewModel Map(GetSectionByIdQueryResponse source)
@@ -19,7 +39,8 @@ namespace SFA.DAS.AODP.Web.Models.Section
                 Order = source.Data.Order,
                 Title = source.Data.Title,
                 FormVersionId = source.Data.FormVersionId,
-                SectionId = source.Data.Id
+                SectionId = source.Data.Id,
+                Pages = source.Data.Pages != null ? [..source.Data.Pages] : new()
             };
         }
     }
