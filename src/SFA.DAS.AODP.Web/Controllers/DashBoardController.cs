b0161d9b083e8335d96f7244df4856550e535fec
Faisal ASHRAF <Faisal.ASHRAF@EDUCATION.GOV.UK>
2025-01-30 19:16:55 +0000

    DFElogin middleware configured.  /Dashboard has authorize attribute as a test. Redirect to dfelogin returns claims after successful login. Config comes from table storage.  Issue with no roles returned. Additonal call is made by dfe login that Requires subject ID from the claims. Clear cookies after each login at the moment to restart authentication.

---
+++ src/SFA.DAS.AODP.Web/Controllers/DashBoardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.AODP.Web.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
