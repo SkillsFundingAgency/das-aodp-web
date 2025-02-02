fcc56a03f5c7ad4e0b182f4cfa9672fd32c5f364
Peter Roddy <Peter.RODDY@EDUCATION.GOV.UK>
2025-02-02 19:25:45 +0000

    query CQRS

---
+++ src/SFA.DAS.AODP.Application/Queries/Qualifications/GetNewQualificationsQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SFA.DAS.AODP.Application.Queries.Qualifications;
+
+namespace SFA.DAS.AODP.Application.Queries.Test
+{
+    public class GetNewQualificationsQuery : IRequest<GetNewQualificationsQueryResponse>
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+    }
+}
