2c0adb42a12b44d169e00457efc17f265213c0a4
Peter Roddy <Peter.RODDY@EDUCATION.GOV.UK>
2025-02-02 19:26:15 +0000

    view and viewmodel added

---
+++ src/SFA.DAS.AODP.Web/Models/Qualifications/NewQualificationsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.AODP.Web.Models.Qualifications
+{
+    public class NewQualificationsViewModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+    }
+}
