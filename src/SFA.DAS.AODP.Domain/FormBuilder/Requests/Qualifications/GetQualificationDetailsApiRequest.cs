e591e7891f857476420e0f7b2528f1c9cf7a5571
Peter Roddy <Peter.RODDY@EDUCATION.GOV.UK>
2025-02-03 20:54:46 +0000

    refactor CQRS to call api endpoint

---
+++ src/SFA.DAS.AODP.Domain/FormBuilder/Requests/Qualifications/GetQualificationDetailsApiRequest.cs
@@ -0,0 +1,16 @@
+﻿using SFA.DAS.AODP.Domain.Interfaces;
+
+namespace SFA.DAS.AODP.Domain.FormBuilder.Requests.Qualifications
+{
+    public class GetQualificationDetailsApiRequest : IGetApiRequest
+    {
+        private readonly int _id;
+
+        public GetQualificationDetailsApiRequest(int id)
+        {
+            _id = id;
+        }
+
+        public string GetUrl => $"api/new-qualifications/{_id}";
+    }
+}
