a0852048c35b53a6f1f48e23fabfc108502c80f5
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-02-14 16:31:32 +0000

    #AWARD-176 Date, text, number and checkbox inputs added (#40)

--- src/SFA.DAS.AODP.Application/Queries/FormBuilder/Pages/GetPagePreviewByIdQueryResponse.cs
+++ src/SFA.DAS.AODP.Application/Queries/FormBuilder/Pages/GetPagePreviewByIdQueryResponse.cs
@@ -18,7 +18,10 @@ public class GetPagePreviewByIdQueryResponse
         public int Order { get; set; }
 
         public TextInputOptions TextInput { get; set; } = new();
-        public RadioOptions RadioButton { get; set; } = new();
+        public NumberInputOptions NumberInput { get; set; } = new();
+        public CheckboxOptions Checkbox { get; set; } = new();
+        public List<Option> Options { get; set; } = new();
+        public DateInputOptions DateInput { get; set; } = new();
 
     }
 
@@ -28,16 +31,32 @@ public class GetPagePreviewByIdQueryResponse
         public int? MaxLength { get; set; }
 
     }
-
-    public class RadioOptions
+    public class CheckboxOptions
     {
-        public List<RadioOptionItem> MultiChoice { get; set; } = new();
+        public int? MinNumberOfOptions { get; set; }
+        public int? MaxNumberOfOptions { get; set; }
+    }
 
-        public class RadioOptionItem
-        {
-            public Guid Id { get; set; }
-            public string Value { get; set; }
-            public int Order { get; set; }
-        }
+    public class NumberInputOptions
+    {
+        public int? GreaterThanOrEqualTo { get; set; }
+        public int? LessThanOrEqualTo { get; set; }
+        public int? NotEqualTo { get; set; }
+    }
+
+
+    public class Option
+    {
+        public Guid Id { get; set; }
+        public string Value { get; set; }
+        public int Order { get; set; }
+    }
+
+    public class DateInputOptions
+    {
+        public DateOnly? GreaterThanOrEqualTo { get; set; }
+        public DateOnly? LessThanOrEqualTo { get; set; }
+        public bool? MustBeInFuture { get; set; }
+        public bool? MustBeInPast { get; set; }
     }
 }
