139019b7fef2e96a9a2496dfe195cce137d1e97b
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-02-19 12:05:37 +0000

    #AWARD-248 add caching for application pages

--- src/SFA.DAS.AODP.Application/Queries/Application/Section/GetApplicationSectionByIdQueryHandler.cs
+++ src/SFA.DAS.AODP.Application/Queries/Application/Section/GetApplicationSectionByIdQueryHandler.cs
@@ -1,14 +1,17 @@
 ﻿using MediatR;
 using SFA.DAS.AODP.Application;
 using SFA.DAS.AODP.Domain.Interfaces;
+using SFA.DAS.AODP.Infrastructure.Cache;
 
 public class GetApplicationSectionByIdQueryHandler : IRequestHandler<GetApplicationSectionByIdQuery, BaseMediatrResponse<GetApplicationSectionByIdQueryResponse>>
 {
     private readonly IApiClient _apiClient;
+    private readonly ICacheService _cacheService;
 
-    public GetApplicationSectionByIdQueryHandler(IApiClient apiClient)
+    public GetApplicationSectionByIdQueryHandler(IApiClient apiClient, ICacheService cacheService)
     {
         _apiClient = apiClient;
+        _cacheService = cacheService;
     }
 
     public async Task<BaseMediatrResponse<GetApplicationSectionByIdQueryResponse>> Handle(GetApplicationSectionByIdQuery request, CancellationToken cancellationToken)
@@ -17,11 +20,14 @@ public class GetApplicationSectionByIdQueryHandler : IRequestHandler<GetApplicat
         response.Success = false;
         try
         {
-            var result = await _apiClient.Get<GetApplicationSectionByIdQueryResponse>(new GetApplicationSectionByIdApiRequest()
+            var cacheKey = $"{nameof(GetApplicationSectionByIdQueryResponse)}_{request.FormVersionId}_{request.SectionId}";
+            var fetchFunc = async () => await _apiClient.Get<GetApplicationSectionByIdQueryResponse>(new GetApplicationSectionByIdApiRequest()
             {
                 SectionId = request.SectionId,
                 FormVersionId = request.FormVersionId,
             });
+
+            var result = await _cacheService.GetAsync(cacheKey, fetchFunc);
 
             response.Value = result;
             response.Success = true;
