0d6566a13dbd3d5e21bb741b25c2018253a4ef3e
David Rimar <david.rimar@digital.education.gov.uk>
2025-02-21 16:09:33 +0000

    AWARD-247 Form Preview wip

---
+++ src/SFA.DAS.AODP.Application/Queries/Application/Form/GetFormPreviewByIdQueryResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace SFA.DAS.AODP.Application.Queries.Application.Form;
+
+public class GetFormPreviewByIdQueryResponse
+{
+    public Guid ApplicationId { get; set; }
+    public Guid FormVersionId { get; set; }
+
+    public List<Question> Data { get; set; }
+
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+    }
+}
---
+++ src/SFA.DAS.AODP.Web/Models/Application/ApplicationFormPreviewViewModel.cs
@@ -0,0 +1,38 @@
+﻿using SFA.DAS.AODP.Application.Queries.Application.Form;
+
+namespace SFA.DAS.AODP.Web.Models.Application;
+
+public class ApplicationFormPreviewViewModel
+{
+    public Guid OrganisationId { get; set; }
+    public Guid FormVersionId { get; set; }
+    public Guid ApplicationId { get; set; }
+
+    public string FormTitle { get; set; }
+
+    public static ApplicationFormPreviewViewModel Map(GetFormPreviewByIdQueryResponse formPreviewResponse,
+        Guid formVersionId,
+        Guid organisationId,
+        Guid applicationId)
+    {
+        ApplicationFormPreviewViewModel model = new()
+        {
+            ApplicationId = applicationId,
+            OrganisationId = organisationId,
+            FormTitle = "DUMMY TITLE",
+            FormVersionId = formVersionId
+        };
+
+        return model;
+    }
+
+    public class Section
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Order { get; set; }
+        public int PagesRemaining { get; set; }
+        public int SkippedPages { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
