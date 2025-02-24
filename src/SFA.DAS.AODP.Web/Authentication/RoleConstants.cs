eb73b28be20b4fa2937a9929465f54b6ced07e8d
Faisal ASHRAF <Faisal.ASHRAF@EDUCATION.GOV.UK>
2025-02-24 16:07:59 +0000

    Forgot to commit update  to roleconstants file.

--- src/SFA.DAS.AODP.Web/Authentication/RoleConstants.cs
+++ src/SFA.DAS.AODP.Web/Authentication/RoleConstants.cs
@@ -3,14 +3,12 @@
 namespace Authentication;
 static class RoleConstants
 {
-    public const string QFAUUserApprover = "qfau_user_approver";
-    public const string QFAUUserReviewer = "qfau_user_reviewer";
-    public const string IFATEUserReviewer = "ifate_user_reviewer";
-    public const string OFQUALUserReviewer = "ofqual_user_reviewer";
-    public const string AOUser = "ao_user";
-    public const string QFAUAdminFormEditor = "qfau_admin_form_editor";
-    public const string IFATEAdminFormEditor = "ifate_admin_form_editor";
-    public const string QFAUAdminDataEditor = "qfau_admin_data_importer";
-
-
+    public const string QFAUApprover = "qfau_user_approver";
+    public const string QFAUReviewer = "qfau_user_reviewer";
+    public const string IFATEReviewer = "ifate_user_reviewer";
+    public const string OFQUALReviewer = "ofqual_user_reviewer";
+    public const string AOApply = "ao_user";
+    public const string QFAUFormBuilder = "qfau_admin_form_editor";
+    public const string IFATEFormBuilder = "ifate_admin_form_editor";
+    public const string QFAUImport = "qfau_admin_data_importer";
 }
