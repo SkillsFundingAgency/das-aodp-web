8684f240da3a713cd3f2209b0ba9b1fb1d15f94c
pete160682 <peterroddy123@live.co.uk>
2025-02-24 08:34:15 +0000

    CQRS added

---
+++ src/SFA.DAS.AODP.Application/Queries/Qualifications/GetNewQualificationsCSVExportHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using SFA.DAS.AODP.Domain.Interfaces;
+using SFA.DAS.AODP.Domain.Qualifications.Requests;
+
+namespace SFA.DAS.AODP.Application.Queries.Qualifications
+{
+    class GetNewQualificationsCSVExportHandler : IRequestHandler<GetNewQualificationsCSVExportQuery, BaseMediatrResponse<GetNewQualificationsCSVExportResponse>>
+    {
+        private readonly IApiClient _apiClient;
+
+        public GetNewQualificationsCSVExportHandler(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<BaseMediatrResponse<GetNewQualificationsCSVExportResponse>> Handle(GetNewQualificationsCSVExportQuery request, CancellationToken cancellationToken)
+        {
+            var response = new BaseMediatrResponse<GetNewQualificationsCSVExportResponse>();
+            response.Success = false;
+            try
+            {
+                var result = await _apiClient.Get<BaseMediatrResponse<GetNewQualificationsCSVExportResponse>>(new GetNewQualificationCSVExportApiRequest());
+                if (result != null && result.Value != null)
+                {
+                    response.Value = result.Value;
+                    response.Success = true;
+                }
+                else
+                {
+                    response.Success = false;
+                    response.ErrorMessage = "No new qualifications found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
---
+++ src/SFA.DAS.AODP.Domain/Qualifications/Requests/GetNewQualificationCSVExportApiRequest.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.AODP.Domain.Interfaces;
+
+namespace SFA.DAS.AODP.Domain.Qualifications.Requests
+{
+    public class GetNewQualificationCSVExportApiRequest : IGetApiRequest
+    {
+        public string GetUrl => "/api/qualifications/qualifications/export?status=new";
+    }
+}
