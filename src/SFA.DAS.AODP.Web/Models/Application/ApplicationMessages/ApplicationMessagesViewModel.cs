7f22ff208afb8af680b77525faa2d45076012a1e
David Rimar <david.rimar@digital.education.gov.uk>
2025-02-26 17:16:31 +0000

    application messages wip

---
+++ src/SFA.DAS.AODP.Web/Models/Application/ApplicationMessages/ApplicationMessagesViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SFA.DAS.AODP.Web.Models.Application.ApplicationMessages;
+
+public class ApplicationMessagesViewModel
+{
+    public List<ApplicationMessageViewModel> Messages { get; set; } = new();
+}
+
+public class ApplicationMessageViewModel
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public string Status { get; set; }
+    public string Type { get; set; }
+    public DateTime SentAt { get; set; }
+    public string SentByName { get; set; }
+    public string SentByEmail { get; set; }
+}
