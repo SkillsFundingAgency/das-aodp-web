d0be250023f8a3824048650684ca99663aa83173
David Rimar <david.rimar@digital.education.gov.uk>
2025-02-28 10:42:15 +0000

    generalise timeline item variables

--- src/SFA.DAS.AODP.Web/Models/Application/ApplicationMessagesViewModel.cs
+++ src/SFA.DAS.AODP.Web/Models/Application/ApplicationMessagesViewModel.cs
@@ -1,4 +1,6 @@
-﻿using SFA.DAS.AODP.Web.Models.TimelineComponents;
+﻿using SFA.DAS.AODP.Web.Enums;
+using SFA.DAS.AODP.Web.Models.TimelineComponents;
+using System.Globalization;
 
 namespace SFA.DAS.AODP.Web.Models.Application;
 
@@ -10,4 +12,27 @@ public class ApplicationMessagesViewModel
 public class ApplicationMessageViewModel : TimelineItemBase
 {
     public string SentByEmail { get; set; }
+    public override string TimelineTitle
+    {
+        get
+        {
+            // if status is not null, display Status: ___, othwrwise just write out the message type
+            return $"Status: {Status}"; // probably need a mapping of the enum to textual representation later
+        }
+    }
+    public override string TimelineMetadata
+    {
+        get
+        {
+            return $"{SentByName}, {SentAt.ToString("dd MMM yyyy 'at' HH:mm", CultureInfo.InvariantCulture)}";
+        }
+    }
+
+    public override bool ShowText
+    {
+        get
+        {
+            return string.Equals(Status, "MessageSent", StringComparison.OrdinalIgnoreCase); // TODO: Confirm statuses to show Text for
+        }
+    }
 }
--- src/SFA.DAS.AODP.Web/Models/TimelineComponents/TimelineItemBase.cs
+++ src/SFA.DAS.AODP.Web/Models/TimelineComponents/TimelineItemBase.cs
@@ -7,4 +7,7 @@ public abstract class TimelineItemBase
     public string Text { get; set; }
     public DateTime SentAt { get; set; }
     public string SentByName { get; set; }
+    public abstract string TimelineTitle { get; }
+    public abstract string TimelineMetadata { get; }
+    public abstract bool ShowText { get; }
 }
