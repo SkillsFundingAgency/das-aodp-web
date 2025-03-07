d68ded4133f5be8f9fec516c1f38a9027b006ef7
Robert Rybnikar <robert.rybnikar@education.gov.uk>
2025-03-07 12:25:00 +0000

    AWARD-390 Changed qualifications

--- src/SFA.DAS.AODP.Web/Models/Qualifications/ChangedQualificationViewModel.cs
+++ src/SFA.DAS.AODP.Web/Models/Qualifications/ChangedQualificationViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace SFA.DAS.AODP.Web.Models.Qualifications
+﻿using SFA.DAS.AODP.Web.Enums;
+
+namespace SFA.DAS.AODP.Web.Models.Qualifications
 {
     public class ChangedQualificationViewModel
     {
@@ -12,5 +14,51 @@
         public string SectorSubjectArea { get; set; } = string.Empty;
         public string ChangedFieldNames { get; set; } = string.Empty;
         public string Status { get; set; } = string.Empty;
+        public string Priority
+        {
+            get
+            {
+                return MapChangedFieldsToPriority();
+            }
+        }
+
+        private string MapChangedFieldsToPriority()
+        {
+            var priority = "Green";
+            if (!string.IsNullOrWhiteSpace(priority) && Status != ActionTypeEnum.NoActionRequired)
+            {
+                var changedFields = ChangedFieldNames.Split(',').Select(s => s.Trim()).ToList();
+                var redChanges = new List<string>() { "Level", "SSA", "GLH" };
+                var yellowChanges = new List<string>()
+                {
+                    "OrganisationName",
+                    "Title",
+                    "Type",
+                    "TotalCredits",
+                    "GradingType",
+                    "OfferedInEngland",
+                    "PreSixteen",
+                    "SixteenToEighteen",
+                    "EighteenPlus",
+                    "NineteenPlus",
+                    "MinimumGLH",
+                    "TQT",
+                    "OperationalEndDate",
+                    "LastUpdatedDate",
+                    "Version",
+                    "OfferedInternationally"
+                };
+                if (changedFields.Intersect(redChanges).Any())
+                {
+                    priority = "Red";
+                }
+                else if (changedFields.Intersect(yellowChanges).Any())
+                {
+                    priority = "Yellow";
+                }
+
+            }
+            return priority;
+        }
     }
 }
