4e3af31f91720f9ac6f9e69806ee4274900e3807
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-03-09 16:48:02 +0000

    wip

--- src/SFA.DAS.AODP.Application/Commands/Application/Application/CreateApplicationCommandHandler.cs
+++ src/SFA.DAS.AODP.Application/Commands/Application/Application/CreateApplicationCommandHandler.cs
@@ -37,39 +37,3 @@ public class CreateApplicationCommandHandler : IRequestHandler<CreateApplication
         return response;
     }
 }
-
-public class SubmitApplicationCommandHandler : IRequestHandler<SubmitApplicationCommand, BaseMediatrResponse<EmptyResponse>>
-{
-    private readonly IApiClient _apiCLient;
-    public SubmitApplicationCommandHandler(IApiClient apiCLient)
-    {
-        _apiCLient = apiCLient;
-    }
-
-
-    public async Task<BaseMediatrResponse<EmptyResponse>> Handle(SubmitApplicationCommand request, CancellationToken cancellationToken)
-    {
-        var response = new BaseMediatrResponse<EmptyResponse>
-        {
-            Success = false
-        };
-
-        try
-        {
-            await _apiCLient.PutWithResponseCode<EmptyResponse>(new SubmitApplicationApiRequest()
-            {
-                ApplicationId = request.ApplicationId,
-                Data = request
-            });
-
-            response.Success = true;
-        }
-        catch (Exception ex)
-        {
-            response.Success = false;
-            response.ErrorMessage = ex.Message;
-        }
-
-        return response;
-    }
-}
--- src/SFA.DAS.AODP.Domain/Application/Application/SubmitApplicationApiRequest.cs
+++ src/SFA.DAS.AODP.Domain/Application/Application/SubmitApplicationApiRequest.cs
@@ -4,7 +4,7 @@ public class SubmitApplicationApiRequest : IPutApiRequest
 {
     public Guid ApplicationId { get; set; }
 
-    public string PutUrl => $"/api/applications/{ApplicationId}/submit";
+    public string PutUrl => $"api/applications/{ApplicationId}/submit";
 
     public object Data { get; set; }
 
