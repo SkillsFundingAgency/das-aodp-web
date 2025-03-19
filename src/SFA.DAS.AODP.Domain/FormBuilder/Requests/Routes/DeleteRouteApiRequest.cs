1102761681226911bd745b15c594fef0f4e3dc27
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-03-19 11:19:50 +0000

    delete routes added (#102)

---
+++ src/SFA.DAS.AODP.Domain/FormBuilder/Requests/Routes/DeleteRouteApiRequest.cs
@@ -0,0 +1,14 @@
+﻿using SFA.DAS.AODP.Domain.Interfaces;
+
+namespace SFA.DAS.AODP.Domain.FormBuilder.Requests.Routes;
+
+public class DeleteRouteApiRequest : IDeleteApiRequest
+{
+    public Guid PageId { get; set; }
+    public Guid FormVersionId { get; set; }
+    public Guid SectionId { get; set; }
+    public Guid QuestionId { get; set; }
+
+    public string DeleteUrl => $"api/routes/forms/{FormVersionId}/sections/{SectionId}/Pages/{PageId}/Questions/{QuestionId}";
+
+}
