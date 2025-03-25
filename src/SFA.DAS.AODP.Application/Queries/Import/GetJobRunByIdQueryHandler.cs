8b505b3fd916367434ec5d01ae556596728790b4
Robert Rybnikar <Robert.rybnikar@education.gov.uk>
2025-03-25 14:52:51 +0000

    Award 452 trigger data import via web 2 (#128)
    * AWARD-452 Trigger job run via web
    
    * AWARD-452 Unit Tests

---
+++ src/SFA.DAS.AODP.Application/Queries/Import/GetJobRunByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using SFA.DAS.AODP.Domain.Import;
+using SFA.DAS.AODP.Domain.Interfaces;
+
+namespace SFA.DAS.AODP.Application.Queries.Import
+{
+    public class GetJobRunByIdQueryHandler : IRequestHandler<GetJobRunByIdQuery, BaseMediatrResponse<GetJobRunByIdQueryResponse>>
+    {
+        private readonly IApiClient _apiClient;
+
+        public GetJobRunByIdQueryHandler(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<BaseMediatrResponse<GetJobRunByIdQueryResponse>> Handle(GetJobRunByIdQuery query, CancellationToken cancellationToken)
+        {
+            var response = new BaseMediatrResponse<GetJobRunByIdQueryResponse>();
+            response.Success = false;
+            try
+            {
+                var result = await _apiClient.Get<GetJobRunByIdQueryResponse>(new GetJobRunByIdApiRequest(query.Id));               
+
+                response.Value = result;
+                response.Success = true;
+
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
---
+++ src/SFA.DAS.AODP.Application/Queries/Import/GetJobRunsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using SFA.DAS.AODP.Domain.Import;
+using SFA.DAS.AODP.Domain.Interfaces;
+
+namespace SFA.DAS.AODP.Application.Queries.Import;
+
+public class GetJobRunsHandler : IRequestHandler<GetJobRunsQuery, BaseMediatrResponse<GetJobRunsQueryResponse>>
+{
+    private readonly IApiClient _apiClient;
+
+    public GetJobRunsHandler(IApiClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+
+    public async Task<BaseMediatrResponse<GetJobRunsQueryResponse>> Handle(GetJobRunsQuery query, CancellationToken cancellationToken)
+    {
+        var response = new BaseMediatrResponse<GetJobRunsQueryResponse>();
+        response.Success = false;
+
+        try
+        {
+            var result = await _apiClient.Get<GetJobRunsQueryResponse>(new GetJobRunsApiRequest() { JobName = query.JobName });            
+
+            response.Value = result;
+            response.Success = true;
+
+        }
+        catch (Exception ex)
+        {
+            response.ErrorMessage = ex.Message;
+        }
+
+        return response;
+    }
+}
+
