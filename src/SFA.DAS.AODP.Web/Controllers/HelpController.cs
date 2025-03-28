2c505919264a69ca69ed8fd62d093943dd838f7c
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2025-03-28 16:40:01 +0000

    Added cookie controller and javascript

---
+++ src/SFA.DAS.AODP.Web/Controllers/HelpController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AODP.Web.Authentication;
+
+namespace SFA.DAS.AODP.Web.Controllers
+{
+    public class HelpController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        //[Route("cookies", Name = RouteConstants.Cookies)]
+        public IActionResult CookiesPolicy()
+        {
+            return View();
+        }
+    }
+}
