372ef0a36ff9dbfc7249523b1a4e0710edc41329
Ravi Udari <ravi.udari@digital.education.gov.uk>
2025-03-31 16:36:05 +0100

    AWARD-621 Add exit survey functionality and feedback views

---
+++ src/SFA.DAS.AODP.Web/Areas/Apply/Controllers/ExitSurveyController.cs
@@ -0,0 +1,65 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AODP.Web.Authentication;
+using SFA.DAS.AODP.Web.Filters;
+using SFA.DAS.AODP.Web.Models.ExitSurvey;
+using SFA.DAS.AODP.Web.Models.Qualifications.Fundings;
+using System.Reflection;
+using ControllerBase = SFA.DAS.AODP.Web.Controllers.ControllerBase;
+
+namespace SFA.DAS.AODP.Web.Areas.Apply.Controllers
+{
+    [Area("Apply")]
+    [Authorize(Policy = PolicyConstants.IsApplyUser)]
+    [ValidateOrganisation]
+    public class ExitSurveyController : ControllerBase
+    {
+        public ExitSurveyController(IMediator mediator, ILogger<ExitSurveyController> logger) : base(mediator, logger)
+        {
+        }
+
+        [HttpGet]
+        [Route("/survey/{page}")]
+        public async Task<IActionResult> ExitSurveyFeedback(string page)
+        {
+            var model = new ExitSurveyFeedbackViewModel
+            {
+                Page = page
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("/survey/{page}")]
+        public async Task<IActionResult> ExitSurveyFeedback(ExitSurveyFeedbackViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            //Save feedback
+
+            try
+            {
+                return RedirectToAction(nameof(ExitSurveyFeedbackConfirmation));
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+                return View(viewModel);
+            }
+        }
+
+        [HttpGet]
+        [Route("/survey/feedbackConfirmation")]
+        public async Task<IActionResult> ExitSurveyFeedbackConfirmation()
+        {
+            return View();
+        }
+    }
+}
+
+
---
+++ src/SFA.DAS.AODP.Web/Models/ExitSurvey/ExitSurveyFeedbackViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.AODP.Web.Models.ExitSurvey
+{
+    public class ExitSurveyFeedbackViewModel
+    {
+        public string Page { get; set; }
+        public int SatisfactionScore { get; set; }
+        public string Comments { get; set; }
+    }
+}
