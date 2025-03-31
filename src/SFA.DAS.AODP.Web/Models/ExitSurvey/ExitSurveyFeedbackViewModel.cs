672d32aed3d8344019ed23cd2b1d9bf0170d03f7
Ravi Udari <ravi.udari@digital.education.gov.uk>
2025-03-31 17:53:11 +0100

    AWARD-621 Refactor Exit Survey feedback form and view model

--- src/SFA.DAS.AODP.Web/Models/ExitSurvey/ExitSurveyFeedbackViewModel.cs
+++ src/SFA.DAS.AODP.Web/Models/ExitSurvey/ExitSurveyFeedbackViewModel.cs
@@ -1,9 +1,22 @@
-﻿namespace SFA.DAS.AODP.Web.Models.ExitSurvey
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFA.DAS.AODP.Web.Models.ExitSurvey
 {
+    public enum SatisfactionScoreIndex
+    {
+        VeryDissatisfied = 1,
+        Dissatisfied = 2,
+        NeitherSatisfiedOrDissatisfied = 3,
+        Satisfied = 4,
+        VerySatisfied = 5
+    }
+
     public class ExitSurveyFeedbackViewModel
     {
         public string Page { get; set; }
-        public int SatisfactionScore { get; set; }
+        [Required]
+        public SatisfactionScoreIndex SatisfactionScore { get; set; }
+        [Required]
         public string Comments { get; set; }
     }
 }
