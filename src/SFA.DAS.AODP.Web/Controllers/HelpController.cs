cfa693b8b3343184f325e78daa47321718e61c89
poojai1214 <pooja.gaurav@education.gov.uk>
2025-04-02 14:34:20 +0100

    Added the Accessibility page (#157)
    Added the Accessibility statement function in the Controller
    We have common Help controller for both Cookies and Accessiblity statement
    Added the link in the footer
    
    Co-authored-by: Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>

---
+++ src/SFA.DAS.AODP.Web/Controllers/HelpController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.AODP.Web.Controllers
+{
+    [Authorize]
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AccessibilityStatement()
+        {
+            return View();
+        }
+    }
+}
