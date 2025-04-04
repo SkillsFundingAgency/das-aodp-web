810afe08f27ab31a246e6beb2e9eba2d0ea739e1
Ravi Udari <ravi.udari@digital.education.gov.uk>
2025-04-04 16:18:00 +0100

    AWARD-720 OutCome screen ErrorMessage updated

--- src/SFA.DAS.AODP.Web/Models/Qualifications/Fundings/QualificationFundingsOffersOutcomeViewModel.cs
+++ src/SFA.DAS.AODP.Web/Models/Qualifications/Fundings/QualificationFundingsOffersOutcomeViewModel.cs
@@ -9,7 +9,7 @@ namespace SFA.DAS.AODP.Web.Models.Qualifications.Fundings
         public string QualificationReference { get; set; }
         public string? Comments { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "You must select an outcome for funding in order to proceed.")]
         public bool? Approved { get; set; }
     }
 }
