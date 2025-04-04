6036c6f6ae605975cd0c58dff82d00579a456c54
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-04-04 17:00:40 +0100

    Award 417 users are able to attach files when sending a message (#166)

---
+++ src/SFA.DAS.AODP.Web.Test/Areas/Apply/Controllers/ApplicationMessagesControllerTests.cs
@@ -0,0 +1,181 @@
+﻿using AutoFixture;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SFA.DAS.AODP.Application;
+using SFA.DAS.AODP.Application.Commands.Application.Application;
+using SFA.DAS.AODP.Application.Commands.FormBuilder.Routes;
+using SFA.DAS.AODP.Infrastructure.File;
+using SFA.DAS.AODP.Models.Settings;
+using SFA.DAS.AODP.Web.Areas.Apply.Controllers;
+using SFA.DAS.AODP.Web.Areas.Apply.Models;
+using SFA.DAS.AODP.Web.Helpers.File;
+using SFA.DAS.AODP.Web.Helpers.User;
+using SFA.DAS.AODP.Web.Models.FormBuilder.Routing;
+using System.Text;
+
+namespace SFA.DAS.AODP.Web.Test.Areas.Apply.Controllers
+{
+    public class ApplicationMessagesControllerTests
+    {
+        private readonly Fixture _fixture = new();
+        private readonly FormBuilderSettings _formBuilderSettings = new();
+
+        private readonly Mock<IUserHelperService> _userHelperServiceMock = new();
+        private readonly ApplicationMessagesController _controller;
+        private readonly Mock<ILogger<ApplicationMessagesController>> _loggerMock = new();
+        private readonly Mock<IMediator> _mediatorMock = new();
+
+        private readonly Mock<IMessageFileValidationService> _messageFileValidationService = new();
+        private readonly Mock<IFileService> _fileService = new();
+
+
+        public ApplicationMessagesControllerTests()
+        {
+            _controller = new(_mediatorMock.Object, _loggerMock.Object, _userHelperServiceMock.Object, _messageFileValidationService.Object, _formBuilderSettings, _fileService.Object);
+        }
+
+        [Fact]
+        public async Task ApplicationMessages_ValidFiles_Uploaded()
+        {
+            // Arrange
+            var fileName = _fixture.Create<string>();
+            var contentType = _fixture.Create<string>();
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("test"));
+            var formFile = new Mock<IFormFile>();
+
+            formFile.SetupGet(f => f.FileName).Returns(fileName);
+            formFile.SetupGet(f => f.ContentType).Returns(contentType);
+            formFile.Setup(f => f.OpenReadStream()).Returns(stream);
+
+
+            ApplicationMessagesViewModel model = new()
+            {
+                AdditionalActions = new()
+                {
+                    Send = true
+                },
+                Files = new()
+                {
+                    formFile.Object
+                }
+            };
+            var applicationId = Guid.NewGuid();
+            var organisationId = Guid.NewGuid();
+            var formVersionId = Guid.NewGuid();
+
+            var msgResponse = new BaseMediatrResponse<CreateApplicationMessageCommandResponse>()
+            {
+                Success = true,
+                Value = _fixture.Create<CreateApplicationMessageCommandResponse>()
+            };
+
+            var metaDataResponse = new BaseMediatrResponse<GetApplicationMetadataByIdQueryResponse>()
+            {
+                Success = true,
+                Value = _fixture.Create<GetApplicationMetadataByIdQueryResponse>()
+            };
+
+            _mediatorMock.Setup(m => m.Send(It.IsAny<CreateApplicationMessageCommand>(), default)).ReturnsAsync(msgResponse);
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetApplicationMetadataByIdQuery>(), default)).ReturnsAsync(metaDataResponse);
+
+            _controller.TempData = new Mock<ITempDataDictionary>().Object;
+
+            // Act
+            var result = await _controller.ApplicationMessages(model, applicationId, organisationId, formVersionId);
+
+            // Assert
+            Assert.IsType<RedirectToActionResult>(result);
+
+            _fileService.Verify(f => f.UploadFileAsync($"messages/{applicationId}/{msgResponse.Value.Id}", It.IsAny<string>(), It.IsAny<Stream>(), It.IsAny<string>(), It.IsAny<string>()));
+            _fileService.Verify(f => f.UploadFileAsync(It.IsAny<string>(), fileName, It.IsAny<Stream>(), It.IsAny<string>(), It.IsAny<string>()));
+            _fileService.Verify(f => f.UploadFileAsync(It.IsAny<string>(), It.IsAny<string>(), stream, It.IsAny<string>(), It.IsAny<string>()));
+            _fileService.Verify(f => f.UploadFileAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<Stream>(), contentType, It.IsAny<string>()));
+            _fileService.Verify(f => f.UploadFileAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<Stream>(), It.IsAny<string>(), metaDataResponse.Value.Reference.ToString().PadLeft(6, '0')));
+
+        }
+
+
+        [Fact]
+        public async Task ApplicationMessages_InvalidFiles_ErrorViewModelReturned()
+        {
+            // Arrange
+            var formFile = new Mock<IFormFile>();
+
+            ApplicationMessagesViewModel model = new()
+            {
+                AdditionalActions = new()
+                {
+                    Send = true
+                },
+                Files = new()
+                {
+                    formFile.Object
+                }
+            };
+            var applicationId = Guid.NewGuid();
+            var organisationId = Guid.NewGuid();
+            var formVersionId = Guid.NewGuid();
+            _controller.TempData = new Mock<ITempDataDictionary>().Object;
+
+            _messageFileValidationService.Setup(f => f.ValidateFiles(model.Files)).Throws(new Exception("error"));
+
+            // Act
+            var result = await _controller.ApplicationMessages(model, applicationId, organisationId, formVersionId);
+
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var resultViewModel = Assert.IsAssignableFrom<ApplicationMessagesViewModel>(viewResult.ViewData.Model);
+            Assert.True(resultViewModel.AdditionalActions.Preview);
+            Assert.False(_controller.ModelState.IsValid);
+            Assert.Contains("Files", _controller.ModelState.Keys);
+        }
+
+
+        [Fact]
+        public async Task ApplicationMessageFileDownload_Success_ReturnsFile()
+        {
+            // Arrange
+            var applicationId = _fixture.Create<Guid>();
+            var file = $"messages/{applicationId}/";
+
+            var blob = _fixture.Create<UploadedBlob>();
+            string content = "Test file content";
+
+            _fileService.Setup(f => f.GetBlobDetails(file)).ReturnsAsync(blob);
+            _fileService.Setup(fs => fs.OpenReadStreamAsync(It.IsAny<string>()))
+              .ReturnsAsync(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(content)));
+
+            // Act
+            var result = await _controller.ApplicationMessageFileDownload(file, applicationId);
+
+            // Assert
+            var fileResult = Assert.IsType<FileStreamResult>(result);
+            Assert.Equal("application/octet-stream", fileResult.ContentType);
+            Assert.Equal(blob.FileNameWithPrefix, fileResult.FileDownloadName);
+
+            using StreamReader reader = new(fileResult.FileStream);
+            Assert.Equal(content, reader.ReadToEnd());
+        }
+
+        [Fact]
+        public async Task ApplicationMessageFileDownload_InvalidFilePath_ReturnsBadRequest()
+        {
+            // Arrange
+            var applicationId = _fixture.Create<Guid>();
+            var file = $"messages/{Guid.NewGuid()}/";
+
+            // Act
+            var result = await _controller.ApplicationMessageFileDownload(file, applicationId);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+    }
+
+}
--- src/SFA.DAS.AODP.Web/Areas/Review/Controllers/ApplicationMessagesController.cs
+++ src/SFA.DAS.AODP.Web/Areas/Review/Controllers/ApplicationMessagesController.cs
@@ -1,12 +1,17 @@
-﻿using MediatR;
+﻿using Azure;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.AODP.Application.Commands.Application.Application;
 using SFA.DAS.AODP.Application.Queries.Application.Application;
+using SFA.DAS.AODP.Authentication.DfeSignInApi.Models;
+using SFA.DAS.AODP.Infrastructure.File;
+using SFA.DAS.AODP.Models.Settings;
 using SFA.DAS.AODP.Models.Users;
 using SFA.DAS.AODP.Web.Areas.Review.Models.ApplicationMessage;
 using SFA.DAS.AODP.Web.Authentication;
 using SFA.DAS.AODP.Web.Enums;
+using SFA.DAS.AODP.Web.Helpers.File;
 using SFA.DAS.AODP.Web.Helpers.User;
 using ControllerBase = SFA.DAS.AODP.Web.Controllers.ControllerBase;
 
@@ -19,10 +24,17 @@ public class ApplicationMessagesController : ControllerBase
     public enum NotificationKeys { MessageSentBanner, MarkAsReadBanner }
     private readonly IUserHelperService _userHelperService;
     private readonly UserType UserType;
-    public ApplicationMessagesController(IMediator mediator, ILogger<ApplicationMessagesController> logger, IUserHelperService userHelperService) : base(mediator, logger)
+    private readonly FormBuilderSettings _formBuilderSettings;
+    private readonly IMessageFileValidationService _messageFileValidationService;
+    private readonly IFileService _fileService;
+
+    public ApplicationMessagesController(IMediator mediator, ILogger<ApplicationMessagesController> logger, IUserHelperService userHelperService, FormBuilderSettings formBuilderSettings, IMessageFileValidationService messageFileValidationService, IFileService fileService) : base(mediator, logger)
     {
         _userHelperService = userHelperService;
         UserType = userHelperService.GetUserType();
+        _formBuilderSettings = formBuilderSettings;
+        _messageFileValidationService = messageFileValidationService;
+        _fileService = fileService;
     }
 
     [HttpGet]
@@ -33,6 +45,7 @@ public class ApplicationMessagesController : ControllerBase
         var response = await Send(new GetApplicationMessagesByIdQuery(applicationId, UserType.ToString()));
         var messages = response.Messages;
 
+        var timelineFiles = await GetApplicationMessageFilesAsync(applicationId);
         var timelineMessages = new List<ApplicationMessageViewModel>();
 
         foreach (var message in messages)
@@ -46,7 +59,13 @@ public class ApplicationMessagesController : ControllerBase
                 SentByName = message.SentByName,
                 SentByEmail = message.SentByEmail,
                 UserType = UserType,
-                MessageType = message.MessageType
+                MessageType = message.MessageType,
+                Files = timelineFiles.Where(t => t.FullPath.StartsWith($"messages/{applicationId}/{message.MessageId}")).Select(a => new ApplicationMessageViewModel.File()
+                {
+                    FileDisplayName = a.FileNameWithPrefix,
+                    FullPath = a.FullPath,
+                    FormUrl = Url.Action(nameof(ApplicationReviewMessageFileDownload), "ApplicationMessages", new { applicationReviewId })
+                }).ToList()
             });
         }
 
@@ -77,6 +96,7 @@ public class ApplicationMessagesController : ControllerBase
         ShowNotificationIfKeyExists(NotificationKeys.MessageSentBanner.ToString(), ViewNotificationMessageType.Success, "Your message has been sent");
         ShowNotificationIfKeyExists(NotificationKeys.MarkAsReadBanner.ToString(), ViewNotificationMessageType.Success, "All messages have been marked as read.");
 
+        model.FileSettings = _formBuilderSettings;
         return View(model);
     }
 
@@ -84,6 +104,7 @@ public class ApplicationMessagesController : ControllerBase
     [Route("review/{applicationReviewId}/messages")]
     public async Task<IActionResult> ApplicationMessages([FromForm] ApplicationMessagesViewModel model)
     {
+        model.FileSettings = _formBuilderSettings;
         var applicationId = await GetApplicationIdAsync(model.ApplicationReviewId);
 
         if (!ModelState.IsValid)
@@ -109,9 +130,28 @@ public class ApplicationMessagesController : ControllerBase
                     break;
 
                 case var _ when model.AdditionalActions.Send:
-                    string userEmail = _userHelperService.GetUserEmail().ToString();
-                    string userName = _userHelperService.GetUserDisplayName().ToString();
-                    var messageId = await Send(new CreateApplicationMessageCommand(applicationId, model.MessageText, model.SelectedMessageType, UserType.ToString(), userEmail, userName));
+                    if (model.Files != null && model.Files.Count != 0)
+                    {
+                        try
+                        {
+                            _messageFileValidationService.ValidateFiles(model.Files);
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError(ex, "Error validating message files");
+                            ModelState.AddModelError("Files", $"The files validation was not successful: {ex.Message}");
+                            model.AdditionalActions.Preview = true;
+                            return View(model);
+                        }
+                    }
+                    string userEmail = _userHelperService.GetUserEmail();
+                    string userName = _userHelperService.GetUserDisplayName();
+                    var response = await Send(new CreateApplicationMessageCommand(applicationId, model.MessageText, model.SelectedMessageType, UserType.ToString(), userEmail, userName));
+
+                    if (model.Files != null && model.Files.Count != 0)
+                    {
+                        await HandleFileUploadsAsync(applicationId, response.Id, model.Files);
+                    }
 
                     TempData[NotificationKeys.MessageSentBanner.ToString()] = "Your message has been sent";
                     TempData.Remove("PreviewMessage");
@@ -148,7 +188,23 @@ public class ApplicationMessagesController : ControllerBase
 
         TempData[NotificationKeys.MarkAsReadBanner.ToString()] = true;
         return RedirectToAction(nameof(ApplicationMessages), new { model.ApplicationReviewId });
+    }
 
+
+    [HttpPost]
+    [Route("review/{applicationReviewId}/message-file-download")]
+    public async Task<IActionResult> ApplicationReviewMessageFileDownload([FromForm] string filePath, [FromRoute] Guid applicationReviewId)
+    {
+        var applicationId = await GetApplicationIdAsync(applicationReviewId);
+
+        if (!filePath.StartsWith($"messages/{applicationId}/"))
+        {
+            return BadRequest();
+        }
+
+        var file = await _fileService.GetBlobDetails(filePath.ToString());
+        var fileStream = await _fileService.OpenReadStreamAsync(filePath);
+        return File(fileStream, "application/octet-stream", file.FileNameWithPrefix);
     }
 
     private async Task<Guid> GetApplicationIdAsync(Guid applicationReviewId)
@@ -161,5 +217,21 @@ public class ApplicationMessagesController : ControllerBase
             if (UserType == UserType.SkillsEngland && !shared.SharedWithSkillsEngland) throw new Exception("Application not shared with Skills England.");
         }
         return shared.ApplicationId;
+    }
+
+    private async Task HandleFileUploadsAsync(Guid applicationId, Guid messageId, List<IFormFile> files)
+    {
+        var metadata = await Send(new GetApplicationMetadataByIdQuery(applicationId));
+
+        foreach (var file in files ?? [])
+        {
+            using var stream = file.OpenReadStream();
+            await _fileService.UploadFileAsync($"messages/{applicationId}/{messageId}", file.FileName, stream, file.ContentType, metadata.Reference.ToString().PadLeft(6, '0'));
+        }
+    }
+
+    private async Task<List<UploadedBlob>> GetApplicationMessageFilesAsync(Guid applicationId)
+    {
+        return _fileService.ListBlobs($"messages/{applicationId}");
     }
 }
