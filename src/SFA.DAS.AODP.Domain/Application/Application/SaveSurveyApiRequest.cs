2f61ccc8832c42b939bd53333b7e22dc6d9257df
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2025-04-07 11:53:29 +0100

    Award 669 survey save web (#164)
    Award 669 survey save web (#164)

---
+++ src/SFA.DAS.AODP.Domain/Application/Application/SaveSurveyApiRequest.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.AODP.Domain.Interfaces;
+
+public class SaveSurveyApiRequest : IPostApiRequest
+{
+    public string PostUrl => "api/Survey";
+
+    public object Data { get; set; }
+
+}
