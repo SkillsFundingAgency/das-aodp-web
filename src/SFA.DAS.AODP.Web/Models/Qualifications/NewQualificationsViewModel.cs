8ef0d966e63badf2394f7bde9f6497d4eef2f00e
JSON Alexander <jasonalexander.dev@gmail.com>
2025-04-08 14:43:08 +0100

    Added process status filtering on get new qualification.  (#147)
    * Added process status filtering on get new qualification.
    
    * Made process status filtering multiple select.
    
    * Updated qualification details page to use NameOfqual as organisation name
    
    * Adjusted new qualifications status filter heading.
    
    * AWARD-376 change process ids to csv list in query param
    
    * AWARD-376 Code review comments
    
    ---------
    
    Co-authored-by: Robert Rybnikar <robert.rybnikar@education.gov.uk>

--- src/SFA.DAS.AODP.Web/Models/Qualifications/NewQualificationsViewModel.cs
+++ src/SFA.DAS.AODP.Web/Models/Qualifications/NewQualificationsViewModel.cs
@@ -19,8 +19,25 @@ namespace SFA.DAS.AODP.Web.Models.Qualifications
         public PaginationViewModel PaginationViewModel { get; set; }
 
         public JobStatusViewModel JobStatusViewModel { get; set; }
+        public List<ProcessStatus> ProcessStatuses { get; set; } = new List<ProcessStatus>();
 
-        public static NewQualificationsViewModel Map(GetNewQualificationsQueryResponse response, string organisation = "", string qan = "", string name = "")
+        public class ProcessStatus
+        {
+            public Guid Id { get; set; }
+            public string? Name { get; set; }
+            public int? IsOutcomeDecision { get; set; }
+            public static implicit operator ProcessStatus(GetProcessStatusesQueryResponse.ProcessStatus v)
+            {
+                return new ProcessStatus
+                {
+                    Id = v.Id,
+                    Name = v.Name,
+                    IsOutcomeDecision = v.IsOutcomeDecision,
+                };
+            }
+        }
+
+        public static NewQualificationsViewModel Map(GetNewQualificationsQueryResponse response, List<GetProcessStatusesQueryResponse.ProcessStatus> processStatuses, string organisation = "", string qan = "", string name = "")
         {
             var viewModel = new NewQualificationsViewModel();
             viewModel.PaginationViewModel = new PaginationViewModel(response.TotalRecords, response.Skip, response.Take);
@@ -44,6 +61,12 @@ namespace SFA.DAS.AODP.Web.Models.Qualifications
                 LastRunTime = response.Job.LastRunTime,
                 Status = response.Job.Status
             };
+            viewModel.ProcessStatuses = processStatuses.Select(v => new ProcessStatus()
+            {
+                Id = v.Id,
+                Name = v.Name,
+                IsOutcomeDecision = v.IsOutcomeDecision,
+            }).ToList();
 
             return viewModel;
         }
