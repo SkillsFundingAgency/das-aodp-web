a10b9ecff623f95a16cf6a026240aa7349f1c349
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-04-16 09:23:12 +0100

    #AWARD-541 fix accessibility issues with routing screens (#200)
    * add choose page view model
    
    * unit tests added

---
+++ src/SFA.DAS.AODP.Web/Models/FormBuilder/Routing/CreateRouteChooseSectionViewModel.cs
@@ -0,0 +1,47 @@
+﻿using SFA.DAS.AODP.Application.Queries.FormBuilder.Routes;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SFA.DAS.AODP.Web.Models.FormBuilder.Routing
+{
+    public class CreateRouteChooseSectionViewModel
+    {
+        [Required]
+        public Guid FormVersionId { get; set; }
+
+        [DisplayName("Section")]
+        [Required]
+        public Guid? ChosenSectionId { get; set; }
+
+        public List<SectionInformation> Sections { get; set; } = new();
+        public bool AreSectionsEmpty => !Sections.Any();
+
+        public class SectionInformation
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public int Order { get; set; }
+        }
+
+        public static CreateRouteChooseSectionViewModel MapToViewModel(GetAvailableSectionsAndPagesForRoutingQueryResponse response, Guid formVersionId)
+        {
+            CreateRouteChooseSectionViewModel model = new()
+            {
+                FormVersionId = formVersionId,
+                Sections = new()
+            };
+
+            foreach (var section in response.Sections)
+            {
+                model.Sections.Add(new()
+                {
+                    Id = section.Id,
+                    Title = section.Title,
+                    Order = section.Order,
+                });
+            }
+            return model;
+        }
+
+    }
+}
