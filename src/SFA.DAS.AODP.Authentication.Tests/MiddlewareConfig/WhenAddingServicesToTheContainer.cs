0ae768ce9bdcf4697b83a9d748ddb7dfa3bf6ce6
FaisalAshraf-dfe <faisal.ashraf@education.gov.uk>
2025-04-16 16:30:05 +0100

    Fix remaining tests (#208)

--- src/SFA.DAS.AODP.Authentication.Tests/MiddlewareConfig/WhenAddingServicesToTheContainer.cs
+++ src/SFA.DAS.AODP.Authentication.Tests/MiddlewareConfig/WhenAddingServicesToTheContainer.cs
@@ -1,147 +1,104 @@
-//using Microsoft.AspNetCore.Authentication.Cookies;
-//using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Caching.Distributed;
-//using Microsoft.AspNetCore.Authentication;
-//using SFA.DAS.AODP.Authentication.Configuration;
-//using SFA.DAS.AODP.Authentication.Services;
-//using SFA.DAS.AODP.Authentication.DfeSignInApi.JWTHelpers;
-//using SFA.DAS.AODP.Authentication.DfeSignInApi.Client;
-//using SFA.DAS.AODP.Authentication.Extensions;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
+using SFA.DAS.AODP.Authentication.Configuration;
+using SFA.DAS.AODP.Authentication.Services;
+using SFA.DAS.AODP.Authentication.DfeSignInApi.JWTHelpers;
+using SFA.DAS.AODP.Authentication.Interfaces;
+using SFA.DAS.AODP.Authentication.Enums;
+using SFA.DAS.AODP.Authentication.Extensions;
+using SFA.DAS.AODP.Authentication.DfeSignInApi.Client;
 
-//namespace SFA.DAS.DfESignIn.Auth.UnitTests.AppStart;
-
-
-//public class WhenAddingServicesToTheContainer
-//{
-
+namespace SFA.DAS.DfESignIn.Auth.UnitTests.AppStart;
 
 
-//    [Theory]
-//    [InlineData(typeof(DfEOidcConfiguration))]
-//    [InlineData(typeof(IDfESignInService))]
-//    [InlineData(typeof(ITokenDataSerializer))]
-//    [InlineData(typeof(ITokenBuilder))]
-//    [InlineData(typeof(IDFESignInAPIClient))]
-//    [InlineData(typeof(ITicketStore))]
-//    public void Then_The_Dependencies_Are_Correctly_Resolved(Type toResolve)
-//    {
-//        var serviceCollection = new ServiceCollection();
-//        SetupServiceCollection(serviceCollection);
+public class WhenAddingServicesToTheContainer
+{
+    [Theory]
+    [InlineData(typeof(DfEOidcConfiguration))]
+    [InlineData(typeof(IDfESignInService))]
+    [InlineData(typeof(ITokenDataSerializer))]
+    [InlineData(typeof(ITokenBuilder))]
+    [InlineData(typeof(IDFESignInAPIClient))]
+    [InlineData(typeof(ITicketStore))]
+    public void Then_The_Dependencies_Are_Correctly_Resolved(Type toResolve)
+    {
+        var serviceCollection = new ServiceCollection();
+        SetupServiceCollection(serviceCollection);
+        serviceCollection.AddDistributedMemoryCache();
+        var provider = serviceCollection.BuildServiceProvider();
 
-//        var provider = serviceCollection.BuildServiceProvider();
+        var type = provider.GetService(toResolve);
 
-//        var type = provider.GetService(toResolve);
+        Assert.NotNull(type);
+    }
 
-//        Assert.NotNull(type);
-//    }
+    [Fact]
+    public async Task Then_ConfigureDfESignInAuthentication_Should_Have_Expected_AuthenticationCookie()
+    {
+        // Arrange
+        var configuration = GenerateConfiguration();
+        var serviceCollection = new ServiceCollection();
+        SetupServiceCollection(serviceCollection);
+        var expectedAuthSchemeNames = new[] { CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme };
 
-//    [Test]
-//    public void Then_DfELoginSessionConnectionString_IsNull_AddDistributedMemoryCache_RegistersDistributedMemoryCache()
-//    {
-//        // Arrange
-//        var configuration = GenerateConfiguration();
-//        configuration["DfEOidcConfiguration:DfELoginSessionConnectionString"] = string.Empty;
+        // Assert
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var authenticationSchemeProvider = serviceProvider.GetService<IAuthenticationSchemeProvider>();
+        Assert.NotNull(authenticationSchemeProvider);
 
-//        var serviceCollection = new ServiceCollection();
-//        serviceCollection.AddServiceRegistration(configuration, typeof(TestCustomServiceRole), ClientName.ProviderRoatp);
+        var authenticationSchemes = await authenticationSchemeProvider?.GetAllSchemesAsync()!;
+        var authSchemeList = authenticationSchemes?.ToList();
+        var actualAuthSchemeNames = authSchemeList?.Select(args => args.Name);
 
-//        // Assert
-//        var serviceProvider = serviceCollection.BuildServiceProvider();
-//        var distributedCache = serviceProvider.GetService<IDistributedCache>();
-//        Assert.That(distributedCache, Is.Not.Null);
-//        Assert.That(distributedCache, Is.InstanceOf<MemoryDistributedCache>());
-//    }
+        var hasExpected = new HashSet<string>(expectedAuthSchemeNames);
+        var hasActual = new HashSet<string>(actualAuthSchemeNames);
 
-//    //[Test]
-//    //public void Then_DfELoginSessionConnectionString_IsNotNull_AddStackExchangeRedisCache_RegistersDistributedMemoryCache()
-//    //{
-//    //    // Arrange
-//    //    var configuration = GenerateConfiguration();
+        var isSuperSet = hasExpected.IsSupersetOf(expectedAuthSchemeNames);
+        Assert.Multiple(() =>
+        {
+            Assert.NotNull(authSchemeList);
+            Assert.True(isSuperSet);
+        });
+    }
 
-//    //    var serviceCollection = new ServiceCollection();
-//    //    serviceCollection.add
-//    //    serviceCollection.AddServiceRegistration(configuration, typeof(TestCustomServiceRole), ClientName.ProviderRoatp);
-
-//    //    // Assert
-//    //    var serviceProvider = serviceCollection.BuildServiceProvider();
-//    //    var distributedCache = serviceProvider.GetService<IDistributedCache>();
-//    //    Assert.That(distributedCache, Is.Not.Null);
-//    //    Assert.That(distributedCache, Is.InstanceOf<RedisCache>());
-//    //}
-
-//    [Test]
-//    public async Task Then_ConfigureDfESignInAuthentication_Should_Have_Expected_AuthenticationCookie()
-//    {
-//        // Arrange
-//        var configuration = GenerateConfiguration();
-//        var serviceCollection = new ServiceCollection();
-//        serviceCollection.AddAndConfigureDfESignInAuthentication(configuration, $"{typeof(AddServiceRegistrationExtension).Assembly.GetName().Name}.Auth", typeof(TestCustomServiceRole), ClientName.ProviderRoatp);
-//        var expectedAuthSchemeNames = new[] { CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme };
-
-//        // Assert
-//        var serviceProvider = serviceCollection.BuildServiceProvider();
-//        var authenticationSchemeProvider = serviceProvider.GetService<IAuthenticationSchemeProvider>();
-//        Assert.That(authenticationSchemeProvider, Is.Not.Null);
-        
-//        var authenticationSchemes = await authenticationSchemeProvider?.GetAllSchemesAsync()!;
-//        var authSchemeList = authenticationSchemes?.ToList();
-//        var actualAuthSchemeNames = authSchemeList?.Select(args => args.Name).ToArray();
-//        Assert.Multiple(() =>
-//        {
-//            Assert.NotNull(authSchemeList);
-//            bool isSuperset = IsSuperset(actualAuthSchemeNames, expectedAuthSchemeNames);
-
-//            // Assert
-//            Assert.True(isSuperset);
-//        });
+    private static void SetupServiceCollection(IServiceCollection serviceCollection)
+    {
 
 
-//        static bool IsSuperset<T>(T[] superset, T[] subset)
-//        {
-//            HashSet<T> supersetSet = new HashSet<T>(superset);
-//            return subset.All(item => supersetSet.Contains(item));
-//        }
+        serviceCollection.AddTransient(typeof(ICustomServiceRole), typeof(CustomServiceRole));
+        var configuration = GenerateConfiguration();
+        serviceCollection.AddDfeSignIn(configuration, "", typeof(CustomServiceRole));
+    }
 
-//    private static void SetupServiceCollection(IServiceCollection serviceCollection)
-//    {
-//        var configuration = GenerateConfiguration();
-//        serviceCollection.BuildServiceProvider().AddServiceRegistration(configuration, typeof(TestCustomServiceRole), ClientName.ProviderRoatp);
-//    }
+    private static IConfigurationRoot GenerateConfiguration()
+    {
+        var configSource = new MemoryConfigurationSource
+        {
+            InitialData = new List<KeyValuePair<string, string>>
+            {
+                new("DfEOidcConfiguration:BaseUrl", "https://test.com/"),
+                new("DfEOidcConfiguration:ClientId", "1234567"),
+                new("DfEOidcConfiguration:APIServiceSecret", "1234567"),
+                new("DfEOidcConfiguration:KeyVaultIdentifier", "https://test.com/"),
+                new("ProviderSharedUIConfiguration:DashboardUrl", "https://test.com/"),
+                new("DfEOidcConfiguration:DfELoginSessionConnectionString", "https://test.com/"),
+                new("DfEOidcConfiguration:LoginSlidingExpiryTimeOutInMinutes", "30"),
+                new("ResourceEnvironmentName", "test"),
+            }
+        };
 
-//    //private static IConfigurationRoot GenerateConfiguration()
-//    //{
-//    //    var configSource = new MemoryConfigurationSource
-//    //    {
-//    //        InitialData = new List<KeyValuePair<string, string>>
-//    //        {
-//    //            new("DfEOidcConfiguration:BaseUrl", "https://test.com/"),
-//    //            new("DfEOidcConfiguration:ClientId", "1234567"),
-//    //            new("DfEOidcConfiguration:APIServiceSecret", "1234567"),
-//    //            new("DfEOidcConfiguration:KeyVaultIdentifier", "https://test.com/"),
-//    //            new("ProviderSharedUIConfiguration:DashboardUrl", "https://test.com/"),
-//    //            new("DfEOidcConfiguration:DfELoginSessionConnectionString", "https://test.com/"),
-//    //            new("DfEOidcConfiguration:LoginSlidingExpiryTimeOutInMinutes", "30"),
-//    //            new("ResourceEnvironmentName", "test"),
-//    //        }
-//    //    };
+        var provider = new MemoryConfigurationProvider(configSource);
 
-//    //    var provider = new MemoryConfigurationProvider(configSource);
+        return new ConfigurationRoot(new List<IConfigurationProvider> { provider });
+    }
 
-//    //    return new ConfigurationRoot(new List<IConfigurationProvider> { provider });
-//    //}
-
-//    //public class TestCustomClaims : ICustomClaims
-//    //{
-//    //    public IEnumerable<Claim?> GetClaims(TokenValidatedContext tokenValidatedContext)
-//    //    {
-//    //        throw new NotImplementedException();
-//    //    }
-//    //}
-
-//    //public class TestCustomServiceRole : ICustomServiceRole
-//    //{
-//    //    public string RoleClaimType => throw new NotImplementedException();
-//    //    public CustomServiceRoleValueType RoleValueType => CustomServiceRoleValueType.Name;
-//    //}
-//}
+    public class CustomServiceRole : ICustomServiceRole
+    {
+        public string RoleClaimType => "http://schemas.portal.com/service";
+        public CustomServiceRoleValueType RoleValueType => CustomServiceRoleValueType.Code;
+    }
+}
