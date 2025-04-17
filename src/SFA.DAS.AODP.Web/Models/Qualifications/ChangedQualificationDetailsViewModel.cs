4c9a174ec05071382278b3c906675321347a284c
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-04-17 09:21:30 +0100

    handle null changed fields

--- src/SFA.DAS.AODP.Web/Models/Qualifications/ChangedQualificationDetailsViewModel.cs
+++ src/SFA.DAS.AODP.Web/Models/Qualifications/ChangedQualificationDetailsViewModel.cs
@@ -107,7 +107,7 @@ public class ChangedQualificationDetailsViewModel
         var priority = "Green";
         if (!string.IsNullOrWhiteSpace(priority) && Status != ActionTypeEnum.NoActionRequired)
         {
-            var changedFields = ChangedFieldNames?.Split(',').Select(s => s.Trim()).ToList();
+            var changedFields = ChangedFieldNames?.Split(',').Select(s => s.Trim()).ToList() ?? [];
             var redChanges = new List<string>() { "Level", "SSA", "GLH" };
             var yellowChanges = new List<string>()
                 {
