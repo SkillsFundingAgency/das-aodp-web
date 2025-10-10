99d420ce0da02c000a3fb14665c8fbc1835530d4
Karen Hanna <karen@alveryhanna.com>
2025-10-10 15:51:39 +0100

    Skeleton vesion of the output file generation

---
+++ src/SFA.DAS.AODP.Application/Queries/Qualifications/GetQualificationExportFileHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using SFA.DAS.AODP.Domain.Interfaces;
+using SFA.DAS.AODP.Domain.Qualifications.Requests;
+
+namespace SFA.DAS.AODP.Application.Queries.Qualifications
+{
+    public class GetQualificationsExportFileHandler : IRequestHandler<GetQualificationExportFileQuery, BaseMediatrResponse<GetQualificationExportFileResponse>>
+    {
+        private readonly IApiClient _apiClient;
+
+        public GetQualificationsExportFileHandler(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<BaseMediatrResponse<GetQualificationExportFileResponse>> Handle(GetQualificationExportFileQuery request, CancellationToken cancellationToken)
+        {
+            var response = new BaseMediatrResponse<GetQualificationExportFileResponse>();
+
+            try
+            {
+                var result = await _apiClient.Get<GetQualificationExportFileResponse>(new GetQualificationExportFileApiRequest());
+                if(result is null || result.ZipFileContent is null || result.ZipFileContent.Length == 0)
+                {
+                    response.Success = false;
+                    response.ErrorMessage = "Export file not available.";
+                }
+                else
+                {
+                    response.Success = true;
+                    response.Value = result; 
+                }
+
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.ErrorMessage = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
--- src/SFA.DAS.AODP.Web/Areas/Admin/Controllers/OutputFileController.cs
+++ src/SFA.DAS.AODP.Web/Areas/Admin/Controllers/OutputFileController.cs
@@ -1,16 +1,41 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AODP.Application.Queries.Qualifications;
 using SFA.DAS.AODP.Web.Authentication;
+using ControllerBase = SFA.DAS.AODP.Web.Controllers.ControllerBase;
 
 namespace SFA.DAS.AODP.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Authorize(Policy = PolicyConstants.IsAdminImportUser)]
-    public class OutputFileController : Controller
+    public class OutputFileController : ControllerBase
     {
+        public OutputFileController(ILogger<OutputFileController> logger, IMediator mediator) : base(mediator, logger)
+        { 
+        }
+
+
         public async Task<IActionResult> Index()
         {
             return View();
+        }
+
+        [HttpGet("export")]
+        public async Task<IActionResult> GetExport(CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new GetQualificationExportFileQuery(), cancellationToken);
+
+            if (!result.Success || result.Value is null)
+            {
+                return Problem(result.ErrorMessage ?? "Unable to retrieve export file.");
+            }
+
+            return File(
+                fileContents: result.Value.ZipFileContent,
+                contentType: result.Value.ContentType,
+                fileDownloadName: result.Value.FileName
+            );
         }
     }
 }
