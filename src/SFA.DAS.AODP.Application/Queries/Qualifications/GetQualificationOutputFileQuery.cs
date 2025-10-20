1efeefa0d5da960789d32883d8c8e312ba2bb0c7
Karen Hanna <karen@alveryhanna.com>
2025-10-20 14:22:04 +0100

    output file history dev started

--- src/SFA.DAS.AODP.Application/Queries/Qualifications/GetQualificationOutputFileQuery.cs
+++ src/SFA.DAS.AODP.Application/Queries/Qualifications/GetQualificationOutputFileQuery.cs
@@ -4,11 +4,5 @@ namespace SFA.DAS.AODP.Application.Queries.Qualifications
 {
     public class GetQualificationOutputFileQuery: IRequest<BaseMediatrResponse<GetQualificationOutputFileResponse>>
     {
-        public string CurrentUsername { get; set; } = string.Empty;
-
-        public GetQualificationOutputFileQuery(string currentUsername)
-        {
-            CurrentUsername = currentUsername;
-        }
     }
 }
