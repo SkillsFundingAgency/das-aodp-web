564678addd7c97145d040a04af1d625e7b3dba2b
BATTEN, Rich <Rich.BATTEN@EDUCATION.GOV.UK>
2025-11-19 16:04:28 +0000

    AWARD-827 Added unit test for ApplicationsReviewController

--- src/SFA.DAS.AODP.Web.Test/Areas/Review/Controllers/ApplicationsReviewController..cs
+++ src/SFA.DAS.AODP.Web.Test/Areas/Review/Controllers/ApplicationsReviewController..cs
@@ -1,20 +1,26 @@
 ﻿using AutoFixture;
 using Azure.Core;
 using MediatR;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Moq;
 using SFA.DAS.AODP.Application;
 using SFA.DAS.AODP.Application.Queries.FormBuilder.Forms;
 using SFA.DAS.AODP.Domain.Interfaces;
 using SFA.DAS.AODP.Infrastructure.Cache;
 using SFA.DAS.AODP.Infrastructure.File;
+using SFA.DAS.AODP.Models.Application;
+using SFA.DAS.AODP.Models.Settings;
 using SFA.DAS.AODP.Models.Users;
 using SFA.DAS.AODP.Web.Areas.Review.Controllers;
+using SFA.DAS.AODP.Web.Areas.Review.Models.ApplicationsReview;
 using SFA.DAS.AODP.Web.Areas.Review.Models.Home;
 using SFA.DAS.AODP.Web.Helpers.User;
 using SFA.DAS.AODP.Web.Models.FormBuilder.Form;
 using System.IO.Compression;
+using System.Reflection;
 
 namespace SFA.DAS.AODP.Web.Test.Areas.Review.Controllers
 {
@@ -26,8 +32,92 @@ namespace SFA.DAS.AODP.Web.Test.Areas.Review.Controllers
         private readonly Mock<IUserHelperService> _userHelperServiceMock = new();
         private readonly Mock<IFileService> _fileServiceMock = new();
         private readonly ApplicationsReviewController _controller;
+        private readonly IOptions<AodpConfiguration> _aodpOptions = Options.Create(new AodpConfiguration
+        {
+            FindRegulatedQualificationUrl = "https://find-a-qualification.services.ofqual.gov.uk/qualifications/"
+        });
 
-        public ApplicationsReviewControllerTests() => _controller = new(_loggerMock.Object, _mediatorMock.Object, _userHelperServiceMock.Object, _fileServiceMock.Object);
+        public ApplicationsReviewControllerTests() => _controller = new(_loggerMock.Object, _mediatorMock.Object, _userHelperServiceMock.Object, _fileServiceMock.Object, _aodpOptions);
+
+        [Fact]
+        public async Task IndexMethod_PopulatesAndReturnsViewCorrectly()
+        {
+            //Arrange
+            var expectedUserType = UserType.Ofqual;
+            _userHelperServiceMock.Setup(x => x.GetUserType()).Returns(expectedUserType);
+
+            var expectedModel = new ApplicationsReviewListViewModel
+            {
+                Page = 2,
+                ItemsPerPage = 10,
+                ApplicationSearch = "Test Search",
+                AwardingOrganisationSearch = "Test AO",
+                Status = new List<ApplicationStatus> { ApplicationStatus.InReview, ApplicationStatus.Approved }
+            };
+
+            var applicationId = Guid.NewGuid();
+            var applicationReviewId = Guid.NewGuid();
+
+            var response = new GetApplicationsForReviewQueryResponse
+            {
+                TotalRecordsCount = 1,
+                Applications = new List<GetApplicationsForReviewQueryResponse.Application>
+                {
+                    new GetApplicationsForReviewQueryResponse.Application
+                    {
+                        Id = applicationId,
+                        ApplicationReviewId = applicationReviewId,
+                        Name = "TestApp",
+                        LastUpdated = DateTime.UtcNow,
+                        Reference = 123456,
+                        Qan = "123456",
+                        AwardingOrganisation = "Test Org",
+                        Owner = "TestOwner",
+                        Status = ApplicationStatus.InReview,
+                        NewMessage = false,
+                        FindRegulatedQualificationUrl = _aodpOptions.Value.FindRegulatedQualificationUrl
+                    }
+                }
+            };
+
+            var mediatorResponse = new BaseMediatrResponse<GetApplicationsForReviewQueryResponse>
+            {
+                Success = true,
+                Value = response
+            };
+
+            _mediatorMock.Setup(m => m.Send(It.Is<GetApplicationsForReviewQuery>(q => 
+                q.ReviewUser == expectedUserType.ToString()
+                && q.ApplicationSearch == expectedModel.ApplicationSearch
+                && q.AwardingOrganisationSearch == expectedModel.AwardingOrganisationSearch
+                && q.ApplicationStatuses != null
+                && q.ApplicationStatuses.Count == expectedModel.Status.Count
+                && q.ApplicationStatuses.Contains(expectedModel.Status[0].ToString())
+                && q.ApplicationsWithNewMessages == true
+                && q.Limit == expectedModel.ItemsPerPage
+                && q.Offset == expectedModel.ItemsPerPage * (expectedModel.Page - 1)
+                && q.FindRegulatedQualificationUrl == _aodpOptions.Value.FindRegulatedQualificationUrl
+            ), It.IsAny<CancellationToken>())).ReturnsAsync(mediatorResponse);
+
+            //Act
+            var result = await _controller.Index(expectedModel) as ViewResult;
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+            Assert.IsType<ApplicationsReviewListViewModel>(result?.Model);
+
+            var returnedModel = result?.Model as ApplicationsReviewListViewModel;
+            Assert.Equal(expectedUserType.ToString(), returnedModel?.UserType);
+            Assert.Equal(_aodpOptions.Value.FindRegulatedQualificationUrl, returnedModel?.FindRegulatedQualificationUrl);
+            Assert.Equal(response.TotalRecordsCount, returnedModel?.TotalItems);
+            Assert.Single(returnedModel?.Applications);
+            Assert.Equal(response.Applications[0].Reference, returnedModel?.Applications[0].Reference);
+            Assert.Equal(response.Applications[0].Qan, returnedModel?.Applications[0].Qan);
+            Assert.Equal(response.Applications[0].Name, returnedModel?.Applications[0].Name);
+
+            _mediatorMock.Verify(m => m.Send(It.IsAny<GetApplicationsForReviewQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+            _userHelperServiceMock.Verify(u => u.GetUserType(), Times.Once);
+        }
 
         [Fact]
         public async Task DownloadAllApplicationFiles_Success_ReturnsZipFile()
@@ -43,7 +133,7 @@ namespace SFA.DAS.AODP.Web.Test.Areas.Review.Controllers
             var sharingResponse = new BaseMediatrResponse<GetApplicationReviewSharingStatusByIdQueryResponse>
             {
                 Success = true,
-                Value = new GetApplicationReviewSharingStatusByIdQueryResponse 
+                Value = new GetApplicationReviewSharingStatusByIdQueryResponse
                 {
                     ApplicationId = applicationId,
                     SharedWithOfqual = true,
@@ -62,8 +152,8 @@ namespace SFA.DAS.AODP.Web.Test.Areas.Review.Controllers
 
             _mediatorMock.Setup(m => m.Send(It.Is<GetApplicationReviewSharingStatusByIdQuery>(query => query.ApplicationReviewId == applicationReviewId), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(sharingResponse);
-             _mediatorMock.Setup(m => m.Send(It.Is<GetApplicationMetadataByIdQuery>(query => query.ApplicationId == applicationId), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(applicationMetadata);
+            _mediatorMock.Setup(m => m.Send(It.Is<GetApplicationMetadataByIdQuery>(query => query.ApplicationId == applicationId), It.IsAny<CancellationToken>()))
+               .ReturnsAsync(applicationMetadata);
 
             // Act
             var result = await _controller.DownloadAllApplicationFiles(applicationReviewId);
@@ -93,7 +183,7 @@ namespace SFA.DAS.AODP.Web.Test.Areas.Review.Controllers
             var sharingResponse = new BaseMediatrResponse<GetApplicationReviewSharingStatusByIdQueryResponse>
             {
                 Success = true,
-                Value = new GetApplicationReviewSharingStatusByIdQueryResponse 
+                Value = new GetApplicationReviewSharingStatusByIdQueryResponse
                 {
                     ApplicationId = applicationId,
                     SharedWithOfqual = true,
@@ -116,11 +206,11 @@ namespace SFA.DAS.AODP.Web.Test.Areas.Review.Controllers
             // Arrange
             var applicationReviewId = Guid.NewGuid();
             var applicationId = Guid.NewGuid();
-            var files = new List<UploadedBlob> { new UploadedBlob { FullPath = "file1.txt", FileName= "file1.txt", FileNamePrefix = "Q1" } };
+            var files = new List<UploadedBlob> { new UploadedBlob { FullPath = "file1.txt", FileName = "file1.txt", FileNamePrefix = "Q1" } };
             var sharingResponse = new BaseMediatrResponse<GetApplicationReviewSharingStatusByIdQueryResponse>
             {
                 Success = true,
-                Value = new GetApplicationReviewSharingStatusByIdQueryResponse 
+                Value = new GetApplicationReviewSharingStatusByIdQueryResponse
                 {
                     ApplicationId = applicationId,
                     SharedWithOfqual = true,
@@ -135,7 +225,7 @@ namespace SFA.DAS.AODP.Web.Test.Areas.Review.Controllers
 
             _mediatorMock.Setup(m => m.Send(It.Is<GetApplicationReviewSharingStatusByIdQuery>(query => query.ApplicationReviewId == applicationReviewId), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(sharingResponse);
-            
+
             // Act & Assert
             await Assert.ThrowsAsync<Exception>(() => _controller.DownloadAllApplicationFiles(applicationReviewId));
         }
@@ -146,11 +236,11 @@ namespace SFA.DAS.AODP.Web.Test.Areas.Review.Controllers
             // Arrange
             var applicationReviewId = Guid.NewGuid();
             var applicationId = Guid.NewGuid();
-            var files = new List<UploadedBlob> { new UploadedBlob { FullPath = "file1.txt", FileName= "file1.txt", FileNamePrefix = "Q1" } };
+            var files = new List<UploadedBlob> { new UploadedBlob { FullPath = "file1.txt", FileName = "file1.txt", FileNamePrefix = "Q1" } };
             var sharingResponse = new BaseMediatrResponse<GetApplicationReviewSharingStatusByIdQueryResponse>
             {
                 Success = true,
-                Value = new GetApplicationReviewSharingStatusByIdQueryResponse 
+                Value = new GetApplicationReviewSharingStatusByIdQueryResponse
                 {
                     ApplicationId = applicationId,
                     SharedWithOfqual = true,
@@ -165,7 +255,7 @@ namespace SFA.DAS.AODP.Web.Test.Areas.Review.Controllers
 
             _mediatorMock.Setup(m => m.Send(It.Is<GetApplicationReviewSharingStatusByIdQuery>(query => query.ApplicationReviewId == applicationReviewId), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(sharingResponse);
-            
+
             // Act & Assert
             await Assert.ThrowsAsync<IOException>(() => _controller.DownloadAllApplicationFiles(applicationReviewId));
         }
