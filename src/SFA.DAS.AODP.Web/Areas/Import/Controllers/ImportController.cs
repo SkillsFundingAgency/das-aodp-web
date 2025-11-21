82f5f6227635cf652b69863ad8d0087031e1f546
DAR, Adeel <Adeel.DAR@EDUCATION.GOV.UK>
2025-11-21 09:58:23 +0000

    Changes to test the pipeline build

---
+++ src/SFA.DAS.AODP.Web/Areas/Import/Controllers/ImportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AODP.Web.Authentication;
+
+namespace SFA.DAS.AODP.Web.Areas.Import.Controllers;
+
+[Area("Import")]
+[Authorize(Policy = PolicyConstants.IsInternalReviewUser)]
+[Route("import")]
+public class ImportController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
