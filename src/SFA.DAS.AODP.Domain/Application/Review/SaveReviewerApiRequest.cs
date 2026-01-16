21fe883e00f03ebe4234ad195e8dc6849620dea5
Karen Hanna <karen@alveryhanna.com>
2026-01-16 14:54:21 +0000

    wip

---
+++ src/SFA.DAS.AODP.Domain/Application/Review/SaveReviewerApiRequest.cs
@@ -0,0 +1,7 @@
+﻿using SFA.DAS.AODP.Domain.Interfaces;
+public class SaveReviewerApiRequest(Guid applicationId) : IPutApiRequest
+{
+    public string PutUrl => $"api/applications/{applicationId}/reviewer";
+
+    public object Data { get; set; }
+}
--- src/SFA.DAS.AODP.Web/Areas/Review/Controllers/ApplicationsReviewController.cs
+++ src/SFA.DAS.AODP.Web/Areas/Review/Controllers/ApplicationsReviewController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using SFA.DAS.AODP.Application.Queries.Application.Form;
 using SFA.DAS.AODP.Application.Queries.Review;
+using SFA.DAS.AODP.Application.Queries.Users;
 using SFA.DAS.AODP.Infrastructure.File;
 using SFA.DAS.AODP.Models.Application;
 using SFA.DAS.AODP.Models.Settings;
@@ -14,7 +15,6 @@ using SFA.DAS.AODP.Web.Areas.Review.Models.ApplicationsReview.FundingApproval;
 using SFA.DAS.AODP.Web.Authentication;
 using SFA.DAS.AODP.Web.Enums;
 using SFA.DAS.AODP.Web.Helpers.User;
-using SFA.DAS.AODP.Web.Models.Application;
 using System.IO.Compression;
 using ControllerBase = SFA.DAS.AODP.Web.Controllers.ControllerBase;
 
@@ -26,7 +26,7 @@ namespace SFA.DAS.AODP.Web.Areas.Review.Controllers
     {
         enum UpdateKeys
         {
-            SharingStatusUpdated, QanUpdated, OwnerUpdated
+            SharingStatusUpdated, QanUpdated, OwnerUpdated, ReviewerUpdated
         }
         private readonly IUserHelperService _userHelperService;
         private readonly UserType UserType;
@@ -82,6 +82,7 @@ namespace SFA.DAS.AODP.Web.Areas.Review.Controllers
         {
             var userType = _userHelperService.GetUserType();
             var review = await Send(new GetApplicationForReviewByIdQuery(applicationReviewId));
+            var reviewersList = await Send(new GetUsersQuery());
 
             if (userType == UserType.Ofqual && review.SharedWithOfqual == false) return NotFound();
             if (userType == UserType.SkillsEngland && review.SharedWithSkillsEngland == false) return NotFound();
@@ -89,7 +90,8 @@ namespace SFA.DAS.AODP.Web.Areas.Review.Controllers
             ShowNotificationIfKeyExists(UpdateKeys.SharingStatusUpdated.ToString(), ViewNotificationMessageType.Success, "The application's sharing status has been updated.");
             ShowNotificationIfKeyExists(UpdateKeys.QanUpdated.ToString(), ViewNotificationMessageType.Success, "The application's QAN has been updated.");
             ShowNotificationIfKeyExists(UpdateKeys.OwnerUpdated.ToString(), ViewNotificationMessageType.Success, "The application's owner has been updated.");
-            return View(ApplicationReviewViewModel.Map(review, userType));
+            ShowNotificationIfKeyExists(UpdateKeys.ReviewerUpdated.ToString(), ViewNotificationMessageType.Success, "The application's reviewer has been updated.");
+            return View(ApplicationReviewViewModel.Map(review, userType, reviewersList));
         }
 
         [Authorize(Policy = PolicyConstants.IsInternalReviewUser)]
@@ -351,6 +353,45 @@ namespace SFA.DAS.AODP.Web.Areas.Review.Controllers
             return RedirectToAction(nameof(ViewApplication), new { applicationReviewId = model.ApplicationReviewId });
         }
 
+        [HttpPost]
+        [Route("review/application-reviews/{applicationReviewId}/update-reviewer")]
+        public async Task<IActionResult> UpdateReviewer(UpdateReviewerViewModel model)
+        {
+            var reviewerUpdateResult = await Send(
+                new SaveReviewerCommand()
+                {
+                    ApplicationId = model.ApplicationId,
+                    SentByEmail = _userHelperService.GetUserEmail(),
+                    SentByName = _userHelperService.GetUserDisplayName(),
+                    ReviewerFieldName = model.ReviewerFieldName,
+                    ReviewerValue = model.ReviewerValue,
+                    UserType = _userHelperService.GetUserType().ToString()
+                });
+
+            if (reviewerUpdateResult.DuplicateReviewerError)
+            {
+                ModelState.AddModelError(model.ReviewerFieldName, "Reviewer 1 and 2 must be different people.");
+
+                var review = await Send(new GetApplicationForReviewByIdQuery(model.ApplicationReviewId));
+                var reviewers = await Send(new GetUsersQuery());
+
+                var vm = ApplicationReviewViewModel.Map(review, UserType.Qfau, reviewers);
+                switch (model.ReviewerFieldName)
+                {
+                    case nameof(vm.Reviewer1):
+                        vm.Reviewer1 = model.ReviewerValue;
+                        break;
+                    case nameof(vm.Reviewer2):
+                        vm.Reviewer2 = model.ReviewerValue;
+                        break;
+                }
+                return View(nameof(ViewApplication), vm);
+            }
+
+            TempData[UpdateKeys.ReviewerUpdated.ToString()] = true;
+            return RedirectToAction(nameof(ViewApplication), new { applicationReviewId = model.ApplicationReviewId });
+        }
+
         [Authorize(Roles = RoleConstants.OFQUALReviewer)]
         [HttpGet]
         [Route("review/application-reviews/{applicationReviewId}/ofqual")]
@@ -564,8 +605,9 @@ namespace SFA.DAS.AODP.Web.Areas.Review.Controllers
         {
             var userType = _userHelperService.GetUserType();
             var review = await Send(new GetApplicationForReviewByIdQuery(applicationReviewId));
+            var users = await Send(new GetUsersQuery());
 
-            var vm = ApplicationReviewViewModel.Map(review, userType);
+            var vm = ApplicationReviewViewModel.Map(review, userType, users);
             vm.Qan = attemptedQan;
 
             return View(nameof(ViewApplication), vm);
