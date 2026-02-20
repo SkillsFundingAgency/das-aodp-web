1206d1f2e7eb69a692e70a02b53223cea10429b9
DAR, Adeel <Adeel.DAR@EDUCATION.GOV.UK>
2026-02-20 13:20:18 +0000

    Staging Models

---
+++ src/SFA.DAS.AODP.Application/Queries/Review/Rollover/GetRolloverCandidateQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace SFA.DAS.AODP.Application.Queries.Review.Rollover;
+
+public class GetRolloverCandidateQuery : IRequest<BaseMediatrResponse<GetRolloverCandidateQueryResponse>>
+{
+    
+}
---
+++ src/SFA.DAS.AODP.Application/Queries/Review/Rollover/GetRolloverCandidateQueryResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace SFA.DAS.AODP.Application.Queries.Review.Rollover;
+
+public class GetRolloverCandidateQueryResponse
+{
+    public int CandidateCount { get; set; }
+}
