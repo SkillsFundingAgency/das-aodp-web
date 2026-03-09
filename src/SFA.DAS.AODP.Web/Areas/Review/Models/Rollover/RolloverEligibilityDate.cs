82eda838337abbe6ebee3fbc24b40bab907977bc
rakib-khan-dfe <rakib.khan@education.gov.uk>
2026-03-09 20:30:59 +0000

    [AWARD-1037] - Page for Input Threshold Dates (#270)

---
+++ src/SFA.DAS.AODP.Web/Areas/Review/Models/Rollover/RolloverEligibilityDate.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFA.DAS.AODP.Web.Areas.Review.Models.Rollover;
+
+public class RolloverEligibilityDate
+{
+    [Required]
+    public int? Day { get; set; }
+
+    [Required]
+    public int? Month { get; set; }
+
+    [Required]
+    public int? Year { get; set; }
+
+    public DateTime? ToDateTime()
+    {
+        if (!Day.HasValue || !Month.HasValue || !Year.HasValue)
+            return null;
+
+        try
+        {
+            return new DateTime(Year.Value, Month.Value, Day.Value);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return null;
+        }
+    }
+}
