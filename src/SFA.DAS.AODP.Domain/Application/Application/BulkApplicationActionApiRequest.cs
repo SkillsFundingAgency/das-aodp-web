ff2e081777b207d472789faec97e8ce6521e16bd
Karen Hanna <karen@alveryhanna.com>
2026-03-16 13:17:39 +0000

    removed leading slash from api request for application bulk actions

--- src/SFA.DAS.AODP.Domain/Application/Application/BulkApplicationActionApiRequest.cs
+++ src/SFA.DAS.AODP.Domain/Application/Application/BulkApplicationActionApiRequest.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.AODP.Domain.Application.Application;
 public class BulkApplicationActionApiRequest : IPutApiRequest
 {
     public Guid ApplicationId { get; set; }
-    public string PutUrl => "/api/application-reviews/bulk-action";
+    public string PutUrl => "api/application-reviews/bulk-action";
     public object Data { get; set; }
 
 }
