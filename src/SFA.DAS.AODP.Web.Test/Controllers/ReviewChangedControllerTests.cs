2498d4f51000b281d6332a038c8a2734fdad0b0d
hamzahshakeel-DfE <hamzah.shakeel@education.gov.uk>
2026-05-27 11:52:37 +0100

    [AWARD-1222] - Fix issue with changed fields not displaying changes correctly (#323)
    * changed the way changed fields are displayed such that key fields are only shown and anything else is disregarded and fixed some formatting issues
    
    also started to move things around such that key field definitions are centralised
    
    * updated packages and removed fluent assertions in favour of shouldl
    
    * small change
    
    * fixed sq issues
    
    * fixed SQ
    
    * added pragma

--- src/SFA.DAS.AODP.Web.Test/Controllers/ReviewChangedControllerTests.cs
+++ src/SFA.DAS.AODP.Web.Test/Controllers/ReviewChangedControllerTests.cs
@@ -105,7 +105,7 @@ public class ReviewChangedControllerTests
         Assert.Equal(2, model.ChangedQualifications.Count);
         Assert.Equal(queryResponse.Value.Data[0].Subject, model.ChangedQualifications[0].Subject);
         Assert.Equal(queryResponse.Value.Data[0].Status, model.ChangedQualifications[0].Status);
-        Assert.Equal(queryResponse.Value.Data[0].AwardingOrganisation, model.ChangedQualifications[0].AwardingOrganisation);
+        Assert.Equal(queryResponse.Value.Data[0].AwardingOrganisation, model.ChangedQualifications[0].AwardingOrganisationName);
         Assert.Equal(queryResponse.Value.Data[0].Status, model.ChangedQualifications[0].Status);
     }
 
@@ -236,7 +236,7 @@ public class ReviewChangedControllerTests
     public async Task Search()
     {
         // Arrange
-        var viewModel = _fixture.Create<ChangedQualificationsViewModel>();
+        var viewModel = new ChangedQualificationsViewModel();
 
         // Act
         var result = await _controller.Search(viewModel);
--- src/SFA.DAS.AODP.Web/Extensions/DateTimeExtensions.cs
+++ src/SFA.DAS.AODP.Web/Extensions/DateTimeExtensions.cs
@@ -1,7 +1,11 @@
-﻿namespace SFA.DAS.AODP.Web.Extensions
+﻿using System.Globalization;
+
+namespace SFA.DAS.AODP.Web.Extensions
 {
     public static class DateTimeExtensions
     {
+        private static readonly CultureInfo LowercaseAmPmCulture = CreateCulture();
+
         public static string ToDateTimeDisplayFormat(this DateTime dateTime)
         {
             return $"{dateTime.ToString("dd MMMM yyyy")} at {dateTime.ToString("hh:mmtt").ToLower()}";
@@ -9,7 +13,36 @@
 
         public static string ToDateDisplayFormat(this DateTime dateTime)
         {
-            return dateTime.ToString("dd MMMM yyyy");
+            return dateTime.ToString("dd MMMM yyyy", CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Formats as a 12 hour date time format with lowercase am/pm designators, e.g. "31 December 2024, 11:59:59 pm"
+        /// </summary>
+        /// <param name="dateTime">The datetime to format.</param>
+        /// <returns>The formatted string.</returns>
+        public static string ToStandard12HourDateTimeFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("dd MMMM yyyy, hh:mm:ss tt", LowercaseAmPmCulture);
+        }
+
+        /// <summary>
+        /// Formats as a long date time format, e.g. "31 December 2024"
+        /// </summary>
+        /// <param name="date">The date to format.</param>
+        /// <returns>The formatted string.</returns>
+        public static string ToStandardDateFormat(this DateOnly date)
+        {
+            return date.ToString("dd MMMM yyyy", CultureInfo.InvariantCulture);
+        }
+
+        private static CultureInfo CreateCulture()
+        {
+            var culture = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+            culture.DateTimeFormat.AMDesignator = "am";
+            culture.DateTimeFormat.PMDesignator = "pm";
+
+            return culture;
         }
     }
 }
