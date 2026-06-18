bf7ec05dbe88fc2a79089ee6a5f846137b26ea83
Hamzah Shakeel <hamzah.shakeel@education.gov.uk>
2026-06-18 08:54:40 +0100

    moved thingd

---
+++ src/SFA.DAS.AODP.Web/Areas/Review/Models/Rollover/ValueObjects/QualificationTypeJsonConverter.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SFA.DAS.AODP.Web.Areas.Review.Models.Rollover.ValueObjects;
+
+public sealed class QualificationTypeJsonConverter : JsonConverter<QualificationType>
+{
+    public override QualificationType? Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Null)
+        {
+            return null;
+        }
+
+        if (reader.TokenType != JsonTokenType.String)
+        {
+            throw new JsonException(
+                $"Expected string when deserialising {nameof(QualificationType)}.");
+        }
+
+        var value = reader.GetString();
+
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return QualificationType.None;
+        }
+
+        if (QualificationType.TryParse(value, out var result))
+        {
+            return result;
+        }
+
+        return QualificationType.Unknown;
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        QualificationType value,
+        JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.Name);
+    }
+}
--- src/SFA.DAS.AODP.Web/Areas/Review/Models/Rollover/ValueObjects/SectorSubjectArea.cs
+++ src/SFA.DAS.AODP.Web/Areas/Review/Models/Rollover/ValueObjects/SectorSubjectArea.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace SFA.DAS.AODP.Web.Areas.Review.Models.Rollover.ValueObjects;
 
@@ -103,46 +102,4 @@ public record SectorSubjectArea
     }
 
     public override string ToString() => Name;
-}
-
-public sealed class SectorSubjectAreaJsonConverter : JsonConverter<SectorSubjectArea>
-{
-    public override SectorSubjectArea? Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options)
-    {
-        if (reader.TokenType == JsonTokenType.Null)
-        {
-            return null;
-        }
-
-        if (reader.TokenType != JsonTokenType.String)
-        {
-            throw new JsonException(
-                $"Expected string when deserialising {nameof(QualificationType)}.");
-        }
-
-        var value = reader.GetString();
-
-        if (string.IsNullOrWhiteSpace(value))
-        {
-            return SectorSubjectArea.NotSpecified;
-        }
-
-        if (SectorSubjectArea.TryParse(value, out var result))
-        {
-            return result;
-        }
-
-        return SectorSubjectArea.NotSpecified;
-    }
-
-    public override void Write(
-        Utf8JsonWriter writer,
-        SectorSubjectArea value,
-        JsonSerializerOptions options)
-    {
-        writer.WriteStringValue(value.Name);
-    }
 }
