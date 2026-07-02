d9f93689bda07085d9635546c3a8a1879df4a48e
karen-hanna <50406092+karen-hanna@users.noreply.github.com>
2026-07-02 08:54:07 +0100

    [AWARD-1188] Updated csv file format for uploading rollover candidates (#325)
    * updated column names for uploading rollover candidates
    
    * added timeout to regex.replace
    
    * pr suggestion implemented - import columns used to generate the list of column names fort he view
    
    ---------
    
    Co-authored-by: hamzahshakeel-DfE <hamzah.shakeel@education.gov.uk>

---
+++ src/SFA.DAS.AODP.Web.Test/Areas/Review/Models/QualificationImportColumnsTests.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.AODP.Web.Areas.Review.Models.Rollover;
+
+namespace SFA.DAS.AODP.Web.UnitTests.Areas.Review.Models
+{
+    public class QualificationImportColumnsTests
+    {
+        [Fact]
+        public void ToCommaSeparatedWithAnd_ReturnsExpectedString()
+        {
+            var result = QualificationImportColumns.ColumnNamesForView();
+
+            Assert.Equal(
+                "QualificationName, AwardingOrganisation, QualificationNumber, Level, QualificationType, Subcategory, SectorSubjectArea and Status",
+                result);
+        }
+    }
+}
