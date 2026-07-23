254d7b05393027dcc5a86322e403567ceb3132c1
EEKEUNE <EEKEUNE@SL634550>
2026-07-23 13:19:36 +0100

    Increase coverage for AO consent cookie handling

--- src/SFA.DAS.AODP.Web.Test/Areas/Review/Controllers/ConsentControllerTests.cs
+++ src/SFA.DAS.AODP.Web.Test/Areas/Review/Controllers/ConsentControllerTests.cs
@@ -98,5 +98,100 @@ namespace SFA.DAS.AODP.Web.UnitTests.Areas.Review.Controllers
                 Assert.Contains("true", setCookieHeader);
             });
         }
+
+        [Fact]
+        public void GetConsentCookieName_ReturnsSameCookieName_WhenEmailCaseAndWhitespaceDiffer()
+        {
+            var firstContext = CreateHttpContextWithClaims(new Claim("email", " AO.User@Test.com "));
+            var secondContext = CreateHttpContextWithClaims(new Claim("email", "ao.user@test.com"));
+
+            var firstCookieName = ConsentController.GetConsentCookieName(firstContext);
+            var secondCookieName = ConsentController.GetConsentCookieName(secondContext);
+
+            Assert.Multiple(() =>
+            {
+                Assert.NotEqual(ConsentController.ConsentCookieName, firstCookieName);
+                Assert.Equal(firstCookieName, secondCookieName);
+            });
+        }
+
+        [Fact]
+        public void GetConsentCookieName_ReturnsDifferentCookieNames_ForDifferentUsers()
+        {
+            var firstContext = CreateHttpContextWithClaims(new Claim("email", "first.user@test.com"));
+            var secondContext = CreateHttpContextWithClaims(new Claim("email", "second.user@test.com"));
+
+            var firstCookieName = ConsentController.GetConsentCookieName(firstContext);
+            var secondCookieName = ConsentController.GetConsentCookieName(secondContext);
+
+            Assert.NotEqual(firstCookieName, secondCookieName);
+        }
+
+        [Fact]
+        public void GetConsentCookieName_UsesClaimTypesEmail_WhenEmailClaimIsMissing()
+        {
+            var httpContext = CreateHttpContextWithClaims(new Claim(ClaimTypes.Email, "standard.email@test.com"));
+
+            var cookieName = ConsentController.GetConsentCookieName(httpContext);
+
+            Assert.Multiple(() =>
+            {
+                Assert.StartsWith($"{ConsentController.ConsentCookieName}_", cookieName);
+                Assert.NotEqual(ConsentController.ConsentCookieName, cookieName);
+            });
+        }
+
+        [Fact]
+        public void GetConsentCookieName_UsesNameIdentifier_WhenEmailClaimsAreMissing()
+        {
+            var httpContext = CreateHttpContextWithClaims(new Claim(ClaimTypes.NameIdentifier, "user-identifier"));
+
+            var cookieName = ConsentController.GetConsentCookieName(httpContext);
+
+            Assert.Multiple(() =>
+            {
+                Assert.StartsWith($"{ConsentController.ConsentCookieName}_", cookieName);
+                Assert.NotEqual(ConsentController.ConsentCookieName, cookieName);
+            });
+        }
+
+        [Fact]
+        public void GetConsentCookieName_UsesIdentityName_WhenIdentifierClaimsAreMissing()
+        {
+            var httpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(
+                    [new Claim(ClaimTypes.Name, "identity.name@test.com")],
+                    "test",
+                    ClaimTypes.Name,
+                    ClaimTypes.Role))
+            };
+
+            var cookieName = ConsentController.GetConsentCookieName(httpContext);
+
+            Assert.Multiple(() =>
+            {
+                Assert.StartsWith($"{ConsentController.ConsentCookieName}_", cookieName);
+                Assert.NotEqual(ConsentController.ConsentCookieName, cookieName);
+            });
+        }
+
+        [Fact]
+        public void GetConsentCookieName_ReturnsBaseCookieName_WhenUserIdentifierIsMissing()
+        {
+            var httpContext = new DefaultHttpContext();
+
+            var cookieName = ConsentController.GetConsentCookieName(httpContext);
+
+            Assert.Equal(ConsentController.ConsentCookieName, cookieName);
+        }
+
+        private static HttpContext CreateHttpContextWithClaims(params Claim[] claims)
+        {
+            return new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(claims, "test"))
+            };
+        }
     }
 }
